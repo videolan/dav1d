8e1d06df8244cf40bb6b9ba1f1e46c9a5d3feb34
Ronald S. Bultje <rsbultje@gmail.com>
2018-10-24 14:19:29 -0400

    Revert "Add tile constraint to loopfilter mask fix-up code"
    This reverts commit 6fa9df9c6f6c620ea6f3d9848465adbdf958648f. The
    behaviour shown there only happens when show_existing_frame=1, i.e.
    it was the same bug as 2e991b1479431081bc1634ec91191763d11b05a4.

--- src/lf_apply.c
+++ src/lf_apply.c
@@ -163,7 +163,7 @@ void bytefn(dav1d_loopfilter_sbrow)(const Dav1dFrameContext *const f,
     // fix lpf strength at tile col boundaries
     const uint8_t *lpf_y = &f->lf.tx_lpf_right_edge[0][sby << sbl2];
     const uint8_t *lpf_uv = &f->lf.tx_lpf_right_edge[1][sby << (sbl2 - ss_ver)];
-    for (int tile_col = 1; tile_col < f->frame_hdr.tiling.cols; tile_col++) {
+    for (int tile_col = 1;; tile_col++) {
         x = f->frame_hdr.tiling.col_start_sb[tile_col];
         if ((x << sbl2) >= f->bw) break;
         const int bx4 = x & is_sb64 ? 16 : 0, cbx4 = bx4 >> ss_hor;
