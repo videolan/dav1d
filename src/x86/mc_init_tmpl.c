fef13fd67cf560c91e0e30eab3c4553a88b8ef63
Francois Cartegnie <fcvlcdev@free.fr>
2018-11-30 15:19:15 +0100

    add SSSE3 blend_v

--- src/x86/mc_init_tmpl.c
+++ src/x86/mc_init_tmpl.c
@@ -61,6 +61,7 @@ decl_w_mask_fn(dav1d_w_mask_420_ssse3);
 decl_blend_fn(dav1d_blend_avx2);
 decl_blend_fn(dav1d_blend_ssse3);
 decl_blend_dir_fn(dav1d_blend_v_avx2);
+decl_blend_dir_fn(dav1d_blend_v_ssse3);
 decl_blend_dir_fn(dav1d_blend_h_avx2);
 
 decl_warp8x8_fn(dav1d_warp_affine_8x8_avx2);
@@ -85,6 +86,7 @@ void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
     c->mask = dav1d_mask_ssse3;
     c->w_mask[2] = dav1d_w_mask_420_ssse3;
     c->blend = dav1d_blend_ssse3;
+    c->blend_v = dav1d_blend_v_ssse3;
 #endif
 
     if (!(flags & DAV1D_X86_CPU_FLAG_AVX2))
