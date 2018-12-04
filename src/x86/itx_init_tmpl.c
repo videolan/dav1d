87a377e990f6cd1db9c214a6d33c15accefe4a32
Liwei Wang <liwei@multicorewareinc.com>
2018-12-04 16:23:06 +0800

    Add SSSE3 implementation for the 4x4 blocks in itx
    Cycle times:
    inv_txfm_add_4x4_adst_adst_0_8bpc_c: 445.9
    inv_txfm_add_4x4_adst_adst_0_8bpc_ssse3: 23.7
    inv_txfm_add_4x4_adst_adst_1_8bpc_c: 443.7
    inv_txfm_add_4x4_adst_adst_1_8bpc_ssse3: 52.6
    inv_txfm_add_4x4_adst_dct_0_8bpc_c: 474.5
    inv_txfm_add_4x4_adst_dct_0_8bpc_ssse3: 23.9
    inv_txfm_add_4x4_adst_dct_1_8bpc_c: 482.0
    inv_txfm_add_4x4_adst_dct_1_8bpc_ssse3: 51.1
    inv_txfm_add_4x4_adst_flipadst_0_8bpc_c: 587.2
    inv_txfm_add_4x4_adst_flipadst_0_8bpc_ssse3: 24.0
    inv_txfm_add_4x4_adst_flipadst_1_8bpc_c: 457.2
    inv_txfm_add_4x4_adst_flipadst_1_8bpc_ssse3: 52.8
    inv_txfm_add_4x4_adst_identity_0_8bpc_c: 412.4
    inv_txfm_add_4x4_adst_identity_0_8bpc_ssse3: 43.3
    inv_txfm_add_4x4_adst_identity_1_8bpc_c: 412.0
    inv_txfm_add_4x4_adst_identity_1_8bpc_ssse3: 43.3
    inv_txfm_add_4x4_dct_adst_0_8bpc_c: 467.4
    inv_txfm_add_4x4_dct_adst_0_8bpc_ssse3: 23.2
    inv_txfm_add_4x4_dct_adst_1_8bpc_c: 588.3
    inv_txfm_add_4x4_dct_adst_1_8bpc_ssse3: 48.6
    inv_txfm_add_4x4_dct_dct_0_8bpc_c: 611.5
    inv_txfm_add_4x4_dct_dct_0_8bpc_ssse3: 23.1
    inv_txfm_add_4x4_dct_dct_1_8bpc_c: 576.2
    inv_txfm_add_4x4_dct_dct_1_8bpc_ssse3: 47.6
    inv_txfm_add_4x4_dct_flipadst_0_8bpc_c: 479.5
    inv_txfm_add_4x4_dct_flipadst_0_8bpc_ssse3: 23.4
    inv_txfm_add_4x4_dct_flipadst_1_8bpc_c: 549.3
    inv_txfm_add_4x4_dct_flipadst_1_8bpc_ssse3: 48.3
    inv_txfm_add_4x4_dct_identity_0_8bpc_c: 576.9
    inv_txfm_add_4x4_dct_identity_0_8bpc_ssse3: 25.4
    inv_txfm_add_4x4_dct_identity_1_8bpc_c: 610.7
    inv_txfm_add_4x4_dct_identity_1_8bpc_ssse3: 25.1
    inv_txfm_add_4x4_flipadst_adst_0_8bpc_c: 532.8
    inv_txfm_add_4x4_flipadst_adst_0_8bpc_ssse3: 23.8
    inv_txfm_add_4x4_flipadst_adst_1_8bpc_c: 666.7
    inv_txfm_add_4x4_flipadst_adst_1_8bpc_ssse3: 61.0
    inv_txfm_add_4x4_flipadst_dct_0_8bpc_c: 539.6
    inv_txfm_add_4x4_flipadst_dct_0_8bpc_ssse3: 23.8
    inv_txfm_add_4x4_flipadst_dct_1_8bpc_c: 484.6
    inv_txfm_add_4x4_flipadst_dct_1_8bpc_ssse3: 51.1
    inv_txfm_add_4x4_flipadst_flipadst_0_8bpc_c: 503.1
    inv_txfm_add_4x4_flipadst_flipadst_0_8bpc_ssse3: 23.9
    inv_txfm_add_4x4_flipadst_flipadst_1_8bpc_c: 463.0
    inv_txfm_add_4x4_flipadst_flipadst_1_8bpc_ssse3: 54.0
    inv_txfm_add_4x4_flipadst_identity_0_8bpc_c: 719.9
    inv_txfm_add_4x4_flipadst_identity_0_8bpc_ssse3: 43.0
    inv_txfm_add_4x4_flipadst_identity_1_8bpc_c: 456.8
    inv_txfm_add_4x4_flipadst_identity_1_8bpc_ssse3: 44.1
    inv_txfm_add_4x4_identity_adst_0_8bpc_c: 422.8
    inv_txfm_add_4x4_identity_adst_0_8bpc_ssse3: 42.4
    inv_txfm_add_4x4_identity_adst_1_8bpc_c: 417.1
    inv_txfm_add_4x4_identity_adst_1_8bpc_ssse3: 42.3
    inv_txfm_add_4x4_identity_dct_0_8bpc_c: 435.4
    inv_txfm_add_4x4_identity_dct_0_8bpc_ssse3: 25.7
    inv_txfm_add_4x4_identity_dct_1_8bpc_c: 434.1
    inv_txfm_add_4x4_identity_dct_1_8bpc_ssse3: 25.3
    inv_txfm_add_4x4_identity_flipadst_0_8bpc_c: 528.1
    inv_txfm_add_4x4_identity_flipadst_0_8bpc_ssse3: 40.9
    inv_txfm_add_4x4_identity_flipadst_1_8bpc_c: 720.0
    inv_txfm_add_4x4_identity_flipadst_1_8bpc_ssse3: 41.8
    inv_txfm_add_4x4_identity_identity_0_8bpc_c: 383.2
    inv_txfm_add_4x4_identity_identity_0_8bpc_ssse3: 28.3
    inv_txfm_add_4x4_identity_identity_1_8bpc_c: 378.9
    inv_txfm_add_4x4_identity_identity_1_8bpc_ssse3: 28.2
    inv_txfm_add_4x4_wht_wht_0_8bpc_c: 271.5
    inv_txfm_add_4x4_wht_wht_0_8bpc_ssse3: 34.0
    inv_txfm_add_4x4_wht_wht_1_8bpc_c: 266.0
    inv_txfm_add_4x4_wht_wht_1_8bpc_ssse3: 33.9

--- src/x86/itx_init_tmpl.c
+++ src/x86/itx_init_tmpl.c
@@ -77,7 +77,7 @@ decl_itx_fn(dav1d_inv_txfm_add_dct_dct_64x16_avx2);
 decl_itx_fn(dav1d_inv_txfm_add_dct_dct_64x32_avx2);
 decl_itx_fn(dav1d_inv_txfm_add_dct_dct_64x64_avx2);
 
-decl_itx_fn(dav1d_inv_txfm_add_dct_dct_4x4_ssse3);
+decl_itx17_fns(4, 4, ssse3);
 
 void bitfn(dav1d_itx_dsp_init_x86)(Dav1dInvTxfmDSPContext *const c) {
 #define assign_itx_fn(pfx, w, h, type, type_enum, ext) \
@@ -115,15 +115,13 @@ void bitfn(dav1d_itx_dsp_init_x86)(Dav1dInvTxfmDSPContext *const c) {
     assign_itx16_fn(pfx, w, h, ext); \
     assign_itx_fn(pfx, w, h, wht_wht,           WHT_WHT,           ext)
 
-#define assign_itx_ssse3_fn_8b() \
-    c->itxfm_add[TX_4X4][DCT_DCT] = dav1d_inv_txfm_add_dct_dct_4x4_ssse3;
 
     const unsigned flags = dav1d_get_cpu_flags();
 
     if (!(flags & DAV1D_X86_CPU_FLAG_SSSE3)) return;
 
 #if BITDEPTH == 8
-    assign_itx_ssse3_fn_8b();
+    assign_itx17_fn(, 4, 4, ssse3);
 #endif
 
     if (!(flags & DAV1D_X86_CPU_FLAG_AVX2)) return;
