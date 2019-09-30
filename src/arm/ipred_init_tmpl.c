c76933864d5b66f628e8358d054e375fcad26f46
Martin Storsjö <martin@martin.st>
2019-09-30 14:32:07 +0300

    arm64: ipred: NEON implementation of the cfl_pred functions
    Relative speedup over the C code:
                                 Cortex A53    A72    A73
    cfl_pred_cfl_128_w4_8bpc_neon:    10.81   7.90   9.80
    cfl_pred_cfl_128_w8_8bpc_neon:    18.38  11.15  13.24
    cfl_pred_cfl_128_w16_8bpc_neon:   16.52  10.83  16.00
    cfl_pred_cfl_128_w32_8bpc_neon:    3.27   3.60   3.70
    cfl_pred_cfl_left_w4_8bpc_neon:    9.82   7.38   8.76
    cfl_pred_cfl_left_w8_8bpc_neon:   17.22  10.63  11.97
    cfl_pred_cfl_left_w16_8bpc_neon:  16.03  10.49  15.66
    cfl_pred_cfl_left_w32_8bpc_neon:   3.28   3.61   3.72
    cfl_pred_cfl_top_w4_8bpc_neon:     9.74   7.39   9.29
    cfl_pred_cfl_top_w8_8bpc_neon:    17.48  10.89  12.58
    cfl_pred_cfl_top_w16_8bpc_neon:   16.01  10.62  15.31
    cfl_pred_cfl_top_w32_8bpc_neon:    3.25   3.62   3.75
    cfl_pred_cfl_w4_8bpc_neon:         8.39   6.34   8.04
    cfl_pred_cfl_w8_8bpc_neon:        15.99  10.12  12.42
    cfl_pred_cfl_w16_8bpc_neon:       15.25  10.40  15.12
    cfl_pred_cfl_w32_8bpc_neon:        3.23   3.58   3.71
    
    The C code gets autovectorized for w >= 32, which is why the
    relative speedup looks strange (but the performance of the NEON
    functions is completely as expected).

--- src/arm/ipred_init_tmpl.c
+++ src/arm/ipred_init_tmpl.c
@@ -39,6 +39,11 @@ decl_angular_ipred_fn(dav1d_ipred_smooth_v_neon);
 decl_angular_ipred_fn(dav1d_ipred_smooth_h_neon);
 decl_angular_ipred_fn(dav1d_ipred_filter_neon);
 
+decl_cfl_pred_fn(dav1d_ipred_cfl_neon);
+decl_cfl_pred_fn(dav1d_ipred_cfl_128_neon);
+decl_cfl_pred_fn(dav1d_ipred_cfl_top_neon);
+decl_cfl_pred_fn(dav1d_ipred_cfl_left_neon);
+
 decl_pal_pred_fn(dav1d_pal_pred_neon);
 
 COLD void bitfn(dav1d_intra_pred_dsp_init_arm)(Dav1dIntraPredDSPContext *const c) {
@@ -58,6 +63,11 @@ COLD void bitfn(dav1d_intra_pred_dsp_init_arm)(Dav1dIntraPredDSPContext *const c
     c->intra_pred[SMOOTH_V_PRED] = dav1d_ipred_smooth_v_neon;
     c->intra_pred[SMOOTH_H_PRED] = dav1d_ipred_smooth_h_neon;
     c->intra_pred[FILTER_PRED]   = dav1d_ipred_filter_neon;
+
+    c->cfl_pred[DC_PRED]         = dav1d_ipred_cfl_neon;
+    c->cfl_pred[DC_128_PRED]     = dav1d_ipred_cfl_128_neon;
+    c->cfl_pred[TOP_DC_PRED]     = dav1d_ipred_cfl_top_neon;
+    c->cfl_pred[LEFT_DC_PRED]    = dav1d_ipred_cfl_left_neon;
 
     c->pal_pred                  = dav1d_pal_pred_neon;
 #endif
