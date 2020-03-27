9e36b9b00122364cf9391380c336595d7dc6e957
Ronald S. Bultje <rsbultje@gmail.com>
2020-03-27 08:41:52 -0400

    x86: add AVX2 version of mc.resize()
    resize_8bpc_c: 1637609.7
    resize_8bpc_avx2: 95162.6

--- src/x86/mc_init_tmpl.c
+++ src/x86/mc_init_tmpl.c
@@ -115,6 +115,8 @@ decl_warp8x8t_fn(dav1d_warp_affine_8x8t_sse2);
 decl_emu_edge_fn(dav1d_emu_edge_avx2);
 decl_emu_edge_fn(dav1d_emu_edge_ssse3);
 
+decl_resize_fn(dav1d_resize_avx2);
+
 COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
 #define init_mc_fn(type, name, suffix) \
     c->mc[type] = dav1d_put_##name##_##suffix
@@ -219,6 +221,7 @@ COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
     c->warp8x8t = dav1d_warp_affine_8x8t_avx2;
 
     c->emu_edge = dav1d_emu_edge_avx2;
+    c->resize = dav1d_resize_avx2;
 #endif
 
     if (!(flags & DAV1D_X86_CPU_FLAG_AVX512ICL))
