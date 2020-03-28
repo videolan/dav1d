275e91de9e3c6ec03a08e617c81923d938eaaa7f
Ronald S. Bultje <rsbultje@gmail.com>
2020-03-28 09:22:15 -0400

    x86: add AVX2 versions for filmgrain.fguv_32x32xn[422/444]
    fguv_32x32xn_8bpc_420_csfl0_c: 14568.2
    fguv_32x32xn_8bpc_420_csfl0_avx2: 940.2
    fguv_32x32xn_8bpc_420_csfl1_c: 10682.0
    fguv_32x32xn_8bpc_420_csfl1_avx2: 783.3
    fguv_32x32xn_8bpc_422_csfl0_c: 16370.5
    fguv_32x32xn_8bpc_422_csfl0_avx2: 1557.3
    fguv_32x32xn_8bpc_422_csfl1_c: 11333.8
    fguv_32x32xn_8bpc_422_csfl1_avx2: 902.1
    fguv_32x32xn_8bpc_444_csfl0_c: 12950.1
    fguv_32x32xn_8bpc_444_csfl0_avx2: 822.9
    fguv_32x32xn_8bpc_444_csfl1_c: 8806.7
    fguv_32x32xn_8bpc_444_csfl1_avx2: 708.2

--- src/x86/film_grain_init_tmpl.c
+++ src/x86/film_grain_init_tmpl.c
@@ -41,6 +41,8 @@ decl_generate_grain_uv_fn(dav1d_generate_grain_uv_422_avx2);
 decl_generate_grain_uv_fn(dav1d_generate_grain_uv_444_avx2);
 decl_fgy_32x32xn_fn(dav1d_fgy_32x32xn_avx2);
 decl_fguv_32x32xn_fn(dav1d_fguv_32x32xn_i420_avx2);
+decl_fguv_32x32xn_fn(dav1d_fguv_32x32xn_i422_avx2);
+decl_fguv_32x32xn_fn(dav1d_fguv_32x32xn_i444_avx2);
 
 COLD void bitfn(dav1d_film_grain_dsp_init_x86)(Dav1dFilmGrainDSPContext *const c) {
     const unsigned flags = dav1d_get_cpu_flags();
@@ -65,5 +67,7 @@ COLD void bitfn(dav1d_film_grain_dsp_init_x86)(Dav1dFilmGrainDSPContext *const c
     c->generate_grain_uv[DAV1D_PIXEL_LAYOUT_I444 - 1] = dav1d_generate_grain_uv_444_avx2;
     c->fgy_32x32xn = dav1d_fgy_32x32xn_avx2;
     c->fguv_32x32xn[DAV1D_PIXEL_LAYOUT_I420 - 1] = dav1d_fguv_32x32xn_i420_avx2;
+    c->fguv_32x32xn[DAV1D_PIXEL_LAYOUT_I422 - 1] = dav1d_fguv_32x32xn_i422_avx2;
+    c->fguv_32x32xn[DAV1D_PIXEL_LAYOUT_I444 - 1] = dav1d_fguv_32x32xn_i444_avx2;
 #endif
 }
