eb78dbe877584c28a662103c69ed0956f7bf519f
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2021-05-04 14:03:18 +0200

    x86: Add high bitdepth ipred_{h,v} AVX2 asm

--- src/x86/ipred_init_tmpl.c
+++ src/x86/ipred_init_tmpl.c
@@ -112,9 +112,9 @@ COLD void bitfn(dav1d_intra_pred_dsp_init_x86)(Dav1dIntraPredDSPContext *const c
     init_angular_ipred_fn(DC_128_PRED,   ipred_dc_128,   avx2);
     init_angular_ipred_fn(TOP_DC_PRED,   ipred_dc_top,   avx2);
     init_angular_ipred_fn(LEFT_DC_PRED,  ipred_dc_left,  avx2);
-#if BITDEPTH == 8
     init_angular_ipred_fn(HOR_PRED,      ipred_h,        avx2);
     init_angular_ipred_fn(VERT_PRED,     ipred_v,        avx2);
+#if BITDEPTH == 8
     init_angular_ipred_fn(PAETH_PRED,    ipred_paeth,    avx2);
     init_angular_ipred_fn(SMOOTH_PRED,   ipred_smooth,   avx2);
     init_angular_ipred_fn(SMOOTH_H_PRED, ipred_smooth_h, avx2);
