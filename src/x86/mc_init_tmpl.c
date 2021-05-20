d2a3f5b47436ffe0dc8d0c80b40ac53ca7eac15b
Henrik Gramner <gramner@twoorioles.com>
2021-05-20 16:41:38 +0200

    x86: Add high bitdepth prep_8tap SSSE3 asm

--- src/x86/mc_init_tmpl.c
+++ src/x86/mc_init_tmpl.c
@@ -158,7 +158,6 @@ COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
     init_mc_fn(FILTER_2D_8TAP_SHARP,          8tap_sharp,          ssse3);
     init_mc_fn(FILTER_2D_BILINEAR,            bilin,               ssse3);
 
-#if BITDEPTH == 8
     init_mct_fn(FILTER_2D_8TAP_REGULAR,        8tap_regular,        ssse3);
     init_mct_fn(FILTER_2D_8TAP_REGULAR_SMOOTH, 8tap_regular_smooth, ssse3);
     init_mct_fn(FILTER_2D_8TAP_REGULAR_SHARP,  8tap_regular_sharp,  ssse3);
@@ -168,7 +167,6 @@ COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
     init_mct_fn(FILTER_2D_8TAP_SHARP_REGULAR,  8tap_sharp_regular,  ssse3);
     init_mct_fn(FILTER_2D_8TAP_SHARP_SMOOTH,   8tap_sharp_smooth,   ssse3);
     init_mct_fn(FILTER_2D_8TAP_SHARP,          8tap_sharp,          ssse3);
-#endif
     init_mct_fn(FILTER_2D_BILINEAR,            bilin,               ssse3);
 
 #if BITDEPTH == 8 && ARCH_X86_64
