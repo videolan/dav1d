3ff8f571c910c30d924ac76b0a6010d01f256ba8
Henrik Gramner <gramner@twoorioles.com>
2021-06-11 16:44:59 +0200

    x86: Add high bitdepth warp8x8 SSSE3 asm

--- src/x86/mc_init_tmpl.c
+++ src/x86/mc_init_tmpl.c
@@ -217,7 +217,8 @@ COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
     c->blend = dav1d_blend_16bpc_ssse3;
     c->blend_v = dav1d_blend_v_16bpc_ssse3;
     c->blend_h = dav1d_blend_h_16bpc_ssse3;
-
+    c->warp8x8 = dav1d_warp_affine_8x8_16bpc_ssse3;
+    c->warp8x8t = dav1d_warp_affine_8x8t_16bpc_ssse3;
     c->emu_edge = dav1d_emu_edge_16bpc_ssse3;
 #endif
 
