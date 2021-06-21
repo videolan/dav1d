1cd6537f881afd37393d483ad8a353ee37c31005
Henrik Gramner <gramner@twoorioles.com>
2021-06-21 12:59:22 +0200

    x86: Add high bitdepth paeth ipred SSSE3 asm

--- src/x86/ipred_init_tmpl.c
+++ src/x86/ipred_init_tmpl.c
@@ -78,8 +78,8 @@ COLD void bitfn(dav1d_intra_pred_dsp_init_x86)(Dav1dIntraPredDSPContext *const c
     init_angular_ipred_fn(LEFT_DC_PRED,  ipred_dc_left,  ssse3);
     init_angular_ipred_fn(HOR_PRED,      ipred_h,        ssse3);
     init_angular_ipred_fn(VERT_PRED,     ipred_v,        ssse3);
-#if BITDEPTH == 8
     init_angular_ipred_fn(PAETH_PRED,    ipred_paeth,    ssse3);
+#if BITDEPTH == 8
     init_angular_ipred_fn(SMOOTH_PRED,   ipred_smooth,   ssse3);
     init_angular_ipred_fn(SMOOTH_H_PRED, ipred_smooth_h, ssse3);
     init_angular_ipred_fn(SMOOTH_V_PRED, ipred_smooth_v, ssse3);
