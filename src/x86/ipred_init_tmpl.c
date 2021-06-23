30000457582d8751022b8ff9e5e18dd7835efedf
Henrik Gramner <gramner@twoorioles.com>
2021-06-23 22:33:17 +0200

    x86: Add high bitdepth smooth ipred SSSE3 asm

--- src/x86/ipred_init_tmpl.c
+++ src/x86/ipred_init_tmpl.c
@@ -79,10 +79,10 @@ COLD void bitfn(dav1d_intra_pred_dsp_init_x86)(Dav1dIntraPredDSPContext *const c
     init_angular_ipred_fn(HOR_PRED,      ipred_h,        ssse3);
     init_angular_ipred_fn(VERT_PRED,     ipred_v,        ssse3);
     init_angular_ipred_fn(PAETH_PRED,    ipred_paeth,    ssse3);
-#if BITDEPTH == 8
     init_angular_ipred_fn(SMOOTH_PRED,   ipred_smooth,   ssse3);
     init_angular_ipred_fn(SMOOTH_H_PRED, ipred_smooth_h, ssse3);
     init_angular_ipred_fn(SMOOTH_V_PRED, ipred_smooth_v, ssse3);
+#if BITDEPTH == 8
     init_angular_ipred_fn(FILTER_PRED,   ipred_filter,   ssse3);
 
     init_cfl_pred_fn(DC_PRED,      ipred_cfl,      ssse3);
