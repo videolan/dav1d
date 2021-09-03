6ab2b716ccc6142bac1f062eb29e8c0845b06931
Matthias Dressel <code@deadcode.eu>
2021-09-03 14:53:45 +0200

    x86: Simplify loopfilter init

--- src/x86/loopfilter_init_tmpl.c
+++ src/x86/loopfilter_init_tmpl.c
@@ -42,17 +42,10 @@ COLD void bitfn(dav1d_loop_filter_dsp_init_x86)(Dav1dLoopFilterDSPContext *const
 
     if (!(flags & DAV1D_X86_CPU_FLAG_SSSE3)) return;
 
-#if BITDEPTH == 8
     c->loop_filter_sb[0][0] = BF(dav1d_lpf_h_sb_y, ssse3);
     c->loop_filter_sb[0][1] = BF(dav1d_lpf_v_sb_y, ssse3);
     c->loop_filter_sb[1][0] = BF(dav1d_lpf_h_sb_uv, ssse3);
     c->loop_filter_sb[1][1] = BF(dav1d_lpf_v_sb_uv, ssse3);
-#else
-    c->loop_filter_sb[0][0] = BF(dav1d_lpf_h_sb_y, ssse3);
-    c->loop_filter_sb[0][1] = BF(dav1d_lpf_v_sb_y, ssse3);
-    c->loop_filter_sb[1][0] = BF(dav1d_lpf_h_sb_uv, ssse3);
-    c->loop_filter_sb[1][1] = BF(dav1d_lpf_v_sb_uv, ssse3);
-#endif
 
     if (!(flags & DAV1D_X86_CPU_FLAG_AVX2)) return;
 
