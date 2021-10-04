8fc719c69b44b648da1a9f18905f4f8a7562330e
Henrik Gramner <gramner@twoorioles.com>
2021-10-04 23:21:27 +0200

    x86: Add mc put_bilin AVX-512 (Ice Lake) asm

--- src/x86/mc_init_tmpl.c
+++ src/x86/mc_init_tmpl.c
@@ -264,6 +264,8 @@ COLD void bitfn(dav1d_mc_dsp_init_x86)(Dav1dMCDSPContext *const c) {
         return;
 
 #if BITDEPTH == 8
+    init_mc_fn (FILTER_2D_BILINEAR,            bilin,               avx512icl);
+
     init_mct_fn(FILTER_2D_8TAP_REGULAR,        8tap_regular,        avx512icl);
     init_mct_fn(FILTER_2D_8TAP_REGULAR_SMOOTH, 8tap_regular_smooth, avx512icl);
     init_mct_fn(FILTER_2D_8TAP_REGULAR_SHARP,  8tap_regular_sharp,  avx512icl);
