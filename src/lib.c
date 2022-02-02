videolan/dav1d

commit 753eef833bdd8ff1585c5c858cafeca8fefbb16e

Merge the 3 threading models into a single one
Merges the 3 threading parameters into a single `--threads=` argument.
Frame threading can still be controlled via the `--framedelay=` argument.
Internally, the threading model is now a global thread/task pool design.

Co-authored-by: Ronald S. Bultje <rsbultje@gmail.com>

src/lib.c
@@ -63,9 +63,8 @@ COLD const char *dav1d_version(void) {
 }
 
 COLD void dav1d_default_settings(Dav1dSettings *const s) {
-    s->n_frame_threads = 1;
-    s->n_tile_threads = 1;
-    s->n_postfilter_threads = 1;
+    s->max_frame_delay = 8; // set to 1 to do low-latency decoding
+    s->n_threads = 1;
     s->apply_grain = 1;
     s->allocator.cookie = NULL;
     s->allocator.alloc_picture_callback = dav1d_default_picture_alloc;
@@ -101,12 +100,10 @@ COLD int dav1d_open(Dav1dContext **const c_out, const Dav1dSettings *const s) {
 
     validate_input_or_ret(c_out != NULL, DAV1D_ERR(EINVAL));
     validate_input_or_ret(s != NULL, DAV1D_ERR(EINVAL));
-    validate_input_or_ret(s->n_postfilter_threads >= 1 &&
-                          s->n_postfilter_threads <= DAV1D_MAX_POSTFILTER_THREADS, DAV1D_ERR(EINVAL));
-    validate_input_or_ret(s->n_tile_threads >= 1 &&
-                          s->n_tile_threads <= DAV1D_MAX_TILE_THREADS, DAV1D_ERR(EINVAL));
-    validate_input_or_ret(s->n_frame_threads >= 1 &&
-                          s->n_frame_threads <= DAV1D_MAX_FRAME_THREADS, DAV1D_ERR(EINVAL));
+    validate_input_or_ret(s->n_threads >= 1 &&
+                          s->n_threads <= DAV1D_MAX_THREADS, DAV1D_ERR(EINVAL));
+    validate_input_or_ret(s->max_frame_delay >= 1 &&
+                          s->max_frame_delay <= DAV1D_MAX_FRAME_DELAY, DAV1D_ERR(EINVAL));
     validate_input_or_ret(s->allocator.alloc_picture_callback != NULL,
                           DAV1D_ERR(EINVAL));
     validate_input_or_ret(s->allocator.release_picture_callback != NULL,
@@ -166,44 +163,25 @@ COLD int dav1d_open(Dav1dContext **const c_out, const Dav1dSettings *const s) {
     c->flush = &c->flush_mem;
     atomic_init(c->flush, 0);
 
-    c->n_pfc = s->n_postfilter_threads;
-    c->n_fc = s->n_frame_threads;
-    c->fc = dav1d_alloc_aligned(sizeof(*c->fc) * s->n_frame_threads, 32);
+    c->n_tc = s->n_threads;
+    c->n_fc = imin(s->n_threads, s->max_frame_delay);
+    c->fc = dav1d_alloc_aligned(sizeof(*c->fc) * c->n_fc, 32);
     if (!c->fc) goto error;
-    memset(c->fc, 0, sizeof(*c->fc) * s->n_frame_threads);
+    memset(c->fc, 0, sizeof(*c->fc) * c->n_fc);
 
-    if (c->n_pfc > 1) {
-        c->pfc = dav1d_alloc_aligned(sizeof(*c->pfc) * s->n_postfilter_threads, 32);
-        if (!c->pfc) goto error;
-        memset(c->pfc, 0, sizeof(*c->pfc) * s->n_postfilter_threads);
-        if (pthread_mutex_init(&c->postfilter_thread.lock, NULL)) goto error;
-        if (pthread_cond_init(&c->postfilter_thread.cond, NULL)) {
-            pthread_mutex_destroy(&c->postfilter_thread.lock);
+    c->tc = dav1d_alloc_aligned(sizeof(*c->tc) * s->n_threads, 64);
+    if (!c->tc) goto error;
+    memset(c->tc, 0, sizeof(*c->tc) * s->n_threads);
+    if (c->n_tc > 1) {
+        if (pthread_mutex_init(&c->task_thread.lock, NULL)) goto error;
+        if (pthread_cond_init(&c->task_thread.cond, NULL)) {
+            pthread_mutex_destroy(&c->task_thread.lock);
             goto error;
         }
-        c->postfilter_thread.inited = 1;
-        for (int n = 0; n < s->n_frame_threads; n++) {
-            Dav1dFrameContext *const f = &c->fc[n];
-            if (pthread_cond_init(&f->lf.thread.cond, NULL)) goto error;
-            f->lf.thread.pftd = &c->postfilter_thread;
-            f->lf.thread.done = 1;
-            f->lf.thread.inited = 1;
-        }
-        for (int n = 0; n < s->n_postfilter_threads; ++n) {
-            Dav1dPostFilterContext *const pf = &c->pfc[n];
-            pf->c = c;
-            if (pthread_mutex_init(&pf->td.lock, NULL)) goto error;
-            if (pthread_cond_init(&pf->td.cond, NULL)) {
-                pthread_mutex_destroy(&pf->td.lock);
-                goto error;
-            }
-            if (pthread_create(&pf->td.thread, &thread_attr, dav1d_postfilter_task, pf)) {
-                pthread_cond_destroy(&c->postfilter_thread.cond);
-                pthread_mutex_destroy(&c->postfilter_thread.lock);
-                goto error;
-            }
-            pf->td.inited = 1;
-        }
+        c->task_thread.cur = c->n_fc;
+        atomic_init(&c->task_thread.reset_task_cur, UINT_MAX);
+        atomic_init(&c->task_thread.cond_signaled, 0);
+        c->task_thread.inited = 1;
     }
 
     if (c->n_fc > 1) {
@@ -211,59 +189,34 @@ COLD int dav1d_open(Dav1dContext **const c_out, const Dav1dSettings *const s) {
             calloc(c->n_fc, sizeof(*c->frame_thread.out_delayed));
         if (!c->frame_thread.out_delayed) goto error;
     }
-    for (int n = 0; n < s->n_frame_threads; n++) {
+    for (unsigned n = 0; n < c->n_fc; n++) {
         Dav1dFrameContext *const f = &c->fc[n];
+        if (c->n_tc > 1)
+            if (pthread_cond_init(&f->task_thread.cond, NULL)) goto error;
         f->c = c;
+        f->task_thread.ttd = &c->task_thread;
         f->lf.last_sharpness = -1;
-        f->n_tc = s->n_tile_threads;
-        f->tc = dav1d_alloc_aligned(sizeof(*f->tc) * s->n_tile_threads, 64);
-        if (!f->tc) goto error;
-        memset(f->tc, 0, sizeof(*f->tc) * s->n_tile_threads);
-        if (f->n_tc > 1) {
-            if (pthread_mutex_init(&f->tile_thread.lock, NULL)) goto error;
-            if (pthread_cond_init(&f->tile_thread.cond, NULL)) {
-                pthread_mutex_destroy(&f->tile_thread.lock);
-                goto error;
-            }
-            if (pthread_cond_init(&f->tile_thread.icond, NULL)) {
-                pthread_mutex_destroy(&f->tile_thread.lock);
-                pthread_cond_destroy(&f->tile_thread.cond);
-                goto error;
-            }
-            f->tile_thread.inited = 1;
-        }
-        for (int m = 0; m < s->n_tile_threads; m++) {
-            Dav1dTileContext *const t = &f->tc[m];
-            t->f = f;
-            memset(t->cf_16bpc, 0, sizeof(t->cf_16bpc));
-            if (f->n_tc > 1) {
-                if (pthread_mutex_init(&t->tile_thread.td.lock, NULL)) goto error;
-                if (pthread_cond_init(&t->tile_thread.td.cond, NULL)) {
-                    pthread_mutex_destroy(&t->tile_thread.td.lock);
-                    goto error;
-                }
-                t->tile_thread.fttd = &f->tile_thread;
-                if (pthread_create(&t->tile_thread.td.thread, &thread_attr, dav1d_tile_task, t)) {
-                    pthread_cond_destroy(&t->tile_thread.td.cond);
-                    pthread_mutex_destroy(&t->tile_thread.td.lock);
-                    goto error;
-                }
-                t->tile_thread.td.inited = 1;
-            }
-        }
         dav1d_refmvs_init(&f->rf);
-        if (c->n_fc > 1) {
-            if (pthread_mutex_init(&f->frame_thread.td.lock, NULL)) goto error;
-            if (pthread_cond_init(&f->frame_thread.td.cond, NULL)) {
-                pthread_mutex_destroy(&f->frame_thread.td.lock);
+    }
+
+    for (int m = 0; m < s->n_threads; m++) {
+        Dav1dTaskContext *const t = &c->tc[m];
+        t->f = &c->fc[0];
+        t->task_thread.ttd = &c->task_thread;
+        t->c = c;
+        memset(t->cf_16bpc, 0, sizeof(t->cf_16bpc));
+        if (c->n_tc > 1) {
+            if (pthread_mutex_init(&t->task_thread.td.lock, NULL)) goto error;
+            if (pthread_cond_init(&t->task_thread.td.cond, NULL)) {
+                pthread_mutex_destroy(&t->task_thread.td.lock);
                 goto error;
             }
-            if (pthread_create(&f->frame_thread.td.thread, &thread_attr, dav1d_frame_task, f)) {
-                pthread_cond_destroy(&f->frame_thread.td.cond);
-                pthread_mutex_destroy(&f->frame_thread.td.lock);
+            if (pthread_create(&t->task_thread.td.thread, &thread_attr, dav1d_worker_task, t)) {
+                pthread_cond_destroy(&t->task_thread.td.cond);
+                pthread_mutex_destroy(&t->task_thread.td.lock);
                 goto error;
             }
-            f->frame_thread.td.inited = 1;
+            t->task_thread.td.inited = 1;
         }
     }
     dav1d_refmvs_dsp_init(&c->refmvs_dsp);
@@ -395,15 +348,23 @@ static int drain_picture(Dav1dContext *const c, Dav1dPicture *const out) {
     do {
         const unsigned next = c->frame_thread.next;
         Dav1dFrameContext *const f = &c->fc[next];
-        pthread_mutex_lock(&f->frame_thread.td.lock);
+        pthread_mutex_lock(&c->task_thread.lock);
         while (f->n_tile_data > 0)
-            pthread_cond_wait(&f->frame_thread.td.cond,
-                              &f->frame_thread.td.lock);
-        pthread_mutex_unlock(&f->frame_thread.td.lock);
+            pthread_cond_wait(&f->task_thread.cond,
+                              &f->task_thread.ttd->lock);
         Dav1dThreadPicture *const out_delayed =
             &c->frame_thread.out_delayed[next];
+        if (out_delayed->p.data[0]) {
+            if (atomic_load(&c->task_thread.first) + 1 < c->n_fc)
+                atomic_fetch_add(&c->task_thread.first, 1);
+            else
+                atomic_store(&c->task_thread.first, 0);
+            if (c->task_thread.cur && c->task_thread.cur < c->n_fc)
+                c->task_thread.cur--;
+        }
         if (++c->frame_thread.next == c->n_fc)
             c->frame_thread.next = 0;
+        pthread_mutex_unlock(&c->task_thread.lock);
         if (out_delayed->p.data[0]) {
             const unsigned progress =
                 atomic_load_explicit(&out_delayed->progress[1],
@@ -510,51 +471,43 @@ void dav1d_flush(Dav1dContext *const c) {
     dav1d_ref_dec(&c->content_light_ref);
     dav1d_ref_dec(&c->itut_t35_ref);
 
-    if (c->n_fc == 1 && c->n_pfc == 1) return;
+    if (c->n_fc == 1 && c->n_tc == 1) return;
+    atomic_store(c->flush, 1);
+
+    // stop running tasks in worker threads
+    if (c->n_tc > 1) {
+        pthread_mutex_lock(&c->task_thread.lock);
+        for (unsigned i = 0; i < c->n_tc; i++) {
+            Dav1dTaskContext *const pf = &c->tc[i];
+            while (!pf->task_thread.flushed) {
+                pthread_cond_wait(&pf->task_thread.td.cond, &c->task_thread.lock);
+            }
+        }
+        for (unsigned i = 0; i < c->n_fc; i++) {
+            c->fc[i].task_thread.task_head = NULL;
+            c->fc[i].task_thread.task_tail = NULL;
+            c->fc[i].task_thread.task_cur_prev = NULL;
+        }
+        atomic_init(&c->task_thread.first, 0);
+        c->task_thread.cur = c->n_fc;
+        atomic_store(&c->task_thread.reset_task_cur, INT_MAX);
+        atomic_store(&c->task_thread.cond_signaled, 0);
+        pthread_mutex_unlock(&c->task_thread.lock);
+    }
 
     // wait for threads to complete flushing
-    if (c->n_pfc > 1)
-        pthread_mutex_lock(&c->postfilter_thread.lock);
-    atomic_store(c->flush, 1);
-    if (c->n_pfc > 1) {
-        pthread_cond_broadcast(&c->postfilter_thread.cond);
-        pthread_mutex_unlock(&c->postfilter_thread.lock);
-    }
-    if (c->n_fc == 1) goto skip_ft_flush;
-    for (unsigned n = 0, next = c->frame_thread.next; n < c->n_fc; n++, next++) {
-        if (next == c->n_fc) next = 0;
-        Dav1dFrameContext *const f = &c->fc[next];
-        pthread_mutex_lock(&f->frame_thread.td.lock);
-        if (f->n_tile_data > 0) {
-            while (f->n_tile_data > 0)
-                pthread_cond_wait(&f->frame_thread.td.cond,
-                                  &f->frame_thread.td.lock);
-            assert(!f->cur.data[0]);
+    if (c->n_fc > 1) {
+        for (unsigned n = 0, next = c->frame_thread.next; n < c->n_fc; n++, next++) {
+            if (next == c->n_fc) next = 0;
+            Dav1dFrameContext *const f = &c->fc[next];
+            dav1d_decode_frame_exit(f, 0);
+            f->n_tile_data = 0;
+            Dav1dThreadPicture *out_delayed = &c->frame_thread.out_delayed[next];
+            if (out_delayed->p.data[0]) {
+                dav1d_thread_picture_unref(out_delayed);
+            }
         }
-        pthread_mutex_unlock(&f->frame_thread.td.lock);
-        Dav1dThreadPicture *const out_delayed =
-            &c->frame_thread.out_delayed[next];
-        if (out_delayed->p.data[0])
-            dav1d_thread_picture_unref(out_delayed);
-    }
-    c->frame_thread.next = 0;
-skip_ft_flush:
-    if (c->n_pfc > 1) {
-        for (unsigned i = 0; i < c->n_pfc; ++i) {
-            Dav1dPostFilterContext *const pf = &c->pfc[i];
-            pthread_mutex_lock(&pf->td.lock);
-            if (!pf->flushed)
-                pthread_cond_wait(&pf->td.cond, &pf->td.lock);
-            pf->flushed = 0;
-            pthread_mutex_unlock(&pf->td.lock);
-        }
-        pthread_mutex_lock(&c->postfilter_thread.lock);
-        c->postfilter_thread.tasks = NULL;
-        pthread_mutex_unlock(&c->postfilter_thread.lock);
-        for (unsigned i = 0; i < c->n_fc; ++i) {
-            freep(&c->fc[i].lf.thread.tasks);
-            c->fc[i].lf.thread.num_tasks = 0;
-        }
+        c->frame_thread.next = 0;
     }
     atomic_store(c->flush, 0);
 }
@@ -570,82 +523,42 @@ static COLD void close_internal(Dav1dContext **const c_out, int flush) {
 
     if (flush) dav1d_flush(c);
 
-    if (c->pfc) {
-        struct PostFilterThreadData *pftd = &c->postfilter_thread;
-        if (pftd->inited) {
-            pthread_mutex_lock(&pftd->lock);
-            for (unsigned n = 0; n < c->n_pfc && c->pfc[n].td.inited; n++)
-                c->pfc[n].die = 1;
-            pthread_cond_broadcast(&pftd->cond);
-            pthread_mutex_unlock(&pftd->lock);
-            for (unsigned n = 0; n < c->n_pfc && c->pfc[n].td.inited; n++) {
-                pthread_join(c->pfc[n].td.thread, NULL);
-                pthread_cond_destroy(&c->pfc[n].td.cond);
-                pthread_mutex_destroy(&c->pfc[n].td.lock);
+    if (c->tc) {
+        struct TaskThreadData *ttd = &c->task_thread;
+        if (ttd->inited) {
+            pthread_mutex_lock(&ttd->lock);
+            for (unsigned n = 0; n < c->n_tc && c->tc[n].task_thread.td.inited; n++)
+                c->tc[n].task_thread.die = 1;
+            pthread_cond_broadcast(&ttd->cond);
+            pthread_mutex_unlock(&ttd->lock);
+            for (unsigned n = 0; n < c->n_tc; n++) {
+                Dav1dTaskContext *const pf = &c->tc[n];
+                if (!pf->task_thread.td.inited) break;
+                pthread_join(pf->task_thread.td.thread, NULL);
+                pthread_cond_destroy(&pf->task_thread.td.cond);
+                pthread_mutex_destroy(&pf->task_thread.td.lock);
             }
-            pthread_cond_destroy(&pftd->cond);
-            pthread_mutex_destroy(&pftd->lock);
+            pthread_cond_destroy(&ttd->cond);
+            pthread_mutex_destroy(&ttd->lock);
         }
-        dav1d_free_aligned(c->pfc);
+        dav1d_free_aligned(c->tc);
     }
 
     for (unsigned n = 0; c->fc && n < c->n_fc; n++) {
         Dav1dFrameContext *const f = &c->fc[n];
 
         // clean-up threading stuff
-        if (c->n_fc > 1 && f->frame_thread.td.inited) {
-            pthread_mutex_lock(&f->frame_thread.td.lock);
-            f->frame_thread.die = 1;
-            pthread_cond_signal(&f->frame_thread.td.cond);
-            pthread_mutex_unlock(&f->frame_thread.td.lock);
-            pthread_join(f->frame_thread.td.thread, NULL);
+        if (c->n_fc > 1) {
             freep(&f->frame_thread.b);
             dav1d_freep_aligned(&f->frame_thread.pal_idx);
             dav1d_freep_aligned(&f->frame_thread.cf);
             freep(&f->frame_thread.tile_start_off);
             dav1d_freep_aligned(&f->frame_thread.pal);
             freep(&f->frame_thread.cbi);
-            pthread_mutex_destroy(&f->frame_thread.td.lock);
-            pthread_cond_destroy(&f->frame_thread.td.cond);
+            pthread_cond_destroy(&f->task_thread.cond);
         }
-        if (f->n_tc > 1 && f->tc && f->tile_thread.inited) {
-            pthread_mutex_lock(&f->tile_thread.lock);
-            for (int m = 0; m < f->n_tc; m++) {
-                Dav1dTileContext *const t = &f->tc[m];
-                t->tile_thread.die = 1;
-                // mark not created tile threads as available
-                if (!t->tile_thread.td.inited)
-                    f->tile_thread.available |= 1ULL<<m;
-            }
-            pthread_cond_broadcast(&f->tile_thread.cond);
-            while (f->tile_thread.available != ~0ULL >> (64 - f->n_tc))
-                pthread_cond_wait(&f->tile_thread.icond,
-                                  &f->tile_thread.lock);
-            pthread_mutex_unlock(&f->tile_thread.lock);
-            for (int m = 0; m < f->n_tc; m++) {
-                Dav1dTileContext *const t = &f->tc[m];
-                if (f->n_tc > 1 && t->tile_thread.td.inited) {
-                    pthread_join(t->tile_thread.td.thread, NULL);
-                    pthread_mutex_destroy(&t->tile_thread.td.lock);
-                    pthread_cond_destroy(&t->tile_thread.td.cond);
-                }
-            }
-            pthread_mutex_destroy(&f->tile_thread.lock);
-            pthread_cond_destroy(&f->tile_thread.cond);
-            pthread_cond_destroy(&f->tile_thread.icond);
-            freep(&f->tile_thread.task_idx_to_sby_and_tile_idx);
-        }
-        for (int m = 0; f->ts && m < f->n_ts; m++) {
-            Dav1dTileState *const ts = &f->ts[m];
-            pthread_cond_destroy(&ts->tile_thread.cond);
-            pthread_mutex_destroy(&ts->tile_thread.lock);
-        }
-        if (f->lf.thread.inited) {
-            freep(&f->lf.thread.tasks);
-            pthread_cond_destroy(&f->lf.thread.cond);
-        }
+        freep(&f->task_thread.tasks);
         dav1d_free_aligned(f->ts);
-        dav1d_free_aligned(f->tc);
         dav1d_free_aligned(f->ipred_edge[0]);
         free(f->a);
         free(f->tile);
@@ -653,6 +566,7 @@ static COLD void close_internal(Dav1dContext **const c_out, int flush) {
         free(f->lf.lr_mask);
         free(f->lf.level);
         free(f->lf.tx_lpf_right_edge[0]);
+        free(f->lf.start_of_tile_row);
         dav1d_refmvs_clear(&f->rf);
         dav1d_free_aligned(f->lf.cdef_line_buf);
         dav1d_free_aligned(f->lf.lr_lpf_line[0]);

.gitlab-ci.yml
@@ -459,8 +459,8 @@ test-debian:
         - ninja coverage-xml
         - grep -Eo 'line-rate="[^"]+"' meson-logs/coverage.xml | head -n 1 |
           grep -Eo '[0-9.]+' | awk '{ print "coverage:", $1 * 100 } '
-        - time meson test -v --suite testdata_seek-stress --test-args "--tilethreads 2 --framethreads 1 --pfthreads=2"
-        - time meson test -v --suite testdata_seek-stress --test-args "--tilethreads 2 --framethreads 2 --pfthreads=2"
+        - time meson test -v --suite testdata_seek-stress --test-args "--threads 2 --framedelay 1"
+        - time meson test -v --suite testdata_seek-stress --test-args "--threads 2 --framedelay 2"
     coverage: '/^coverage: (\d+.\d+)$/'
     artifacts:
         expose_as: 'Coverage HTML report'
@@ -570,14 +570,11 @@ test-debian-tsan:
         - ninja -C build
         - cd build
         - exit_code=0
-        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--tilethreads 1 --framethreads 2 --pfthreads 1" || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--tilethreads 2 --framethreads 1 --pfthreads 1" || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--tilethreads 2 --framethreads 2 --pfthreads 1" || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--tilethreads 2 --framethreads 1 --pfthreads 2" || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--tilethreads 2 --framethreads 2 --pfthreads 2" || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata_seek-stress --test-args "--tilethreads 2 --framethreads 1 --pfthreads 2"                               || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite testdata_seek-stress --test-args "--tilethreads 2 --framethreads 2 --pfthreads 2"                               || exit_code=$((exit_code + $?))
-        - time meson test -v --setup=sanitizer --suite oss-fuzz-asan --suite oss-fuzz-msan --suite oss-fuzz-ubsan                                                      || exit_code=$((exit_code + $?))
+        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--threads 2 --framedelay 1" || exit_code=$((exit_code + $?))
+        - time meson test -v --setup=sanitizer --suite testdata-8 --suite testdata-10 --suite testdata-12 --test-args "--threads 2 --framedelay 2" || exit_code=$((exit_code + $?))
+        - time meson test -v --setup=sanitizer --suite testdata_seek-stress --test-args "--threads 2 --framedelay 1"                               || exit_code=$((exit_code + $?))
+        - time meson test -v --setup=sanitizer --suite testdata_seek-stress --test-args "--threads 2 --framedelay 2"                               || exit_code=$((exit_code + $?))
+        - time meson test -v --setup=sanitizer --suite oss-fuzz-asan --suite oss-fuzz-msan --suite oss-fuzz-ubsan                                  || exit_code=$((exit_code + $?))
         - if [ $exit_code -ne 0 ]; then exit $exit_code; fi
 
 test-win64:

examples/dav1dplay.c
@@ -147,9 +147,8 @@ static void dp_rd_ctx_parse_args(Dav1dPlayRenderContext *rd_ctx,
     static const char short_opts[] = "i:vuzgr:";
 
     enum {
-        ARG_FRAME_THREADS = 256,
-        ARG_TILE_THREADS,
-        ARG_POSTFILTER_THREADS,
+        ARG_THREADS = 256,
+        ARG_FRAME_DELAY,
         ARG_HIGH_QUALITY,
     };
 
@@ -158,9 +157,8 @@ static void dp_rd_ctx_parse_args(Dav1dPlayRenderContext *rd_ctx,
         { "input",          1, NULL, 'i' },
         { "version",        0, NULL, 'v' },
         { "untimed",        0, NULL, 'u' },
-        { "framethreads",   1, NULL, ARG_FRAME_THREADS },
-        { "tilethreads",    1, NULL, ARG_TILE_THREADS },
-        { "pfthreads",      1, NULL, ARG_POSTFILTER_THREADS },
+        { "threads",        1, NULL, ARG_THREADS },
+        { "framedelay",     1, NULL, ARG_FRAME_DELAY },
         { "highquality",    0, NULL, ARG_HIGH_QUALITY },
         { "zerocopy",       0, NULL, 'z' },
         { "gpugrain",       0, NULL, 'g' },
@@ -191,17 +189,13 @@ static void dp_rd_ctx_parse_args(Dav1dPlayRenderContext *rd_ctx,
             case 'r':
                 settings->renderer_name = optarg;
                 break;
-            case ARG_FRAME_THREADS:
-                lib_settings->n_frame_threads =
-                    parse_unsigned(optarg, ARG_FRAME_THREADS, argv[0]);
+            case ARG_THREADS:
+                lib_settings->n_threads =
+                    parse_unsigned(optarg, ARG_THREADS, argv[0]);
                 break;
-            case ARG_TILE_THREADS:
-                lib_settings->n_tile_threads =
-                    parse_unsigned(optarg, ARG_TILE_THREADS, argv[0]);
-                break;
-            case ARG_POSTFILTER_THREADS:
-                lib_settings->n_postfilter_threads =
-                    parse_unsigned(optarg, ARG_POSTFILTER_THREADS, argv[0]);
+            case ARG_FRAME_DELAY:
+                lib_settings->max_frame_delay =
+                    parse_unsigned(optarg, ARG_FRAME_DELAY, argv[0]);
                 break;
             default:
                 dp_settings_print_usage(argv[0], NULL);

include/dav1d/dav1d.h
@@ -43,9 +43,8 @@ extern "C" {
 typedef struct Dav1dContext Dav1dContext;
 typedef struct Dav1dRef Dav1dRef;
 
-#define DAV1D_MAX_FRAME_THREADS 256
-#define DAV1D_MAX_TILE_THREADS 64
-#define DAV1D_MAX_POSTFILTER_THREADS 256
+#define DAV1D_MAX_THREADS 256
+#define DAV1D_MAX_FRAME_DELAY 256
 
 typedef struct Dav1dLogger {
     void *cookie; ///< Custom data to pass to the callback.
@@ -60,16 +59,15 @@ typedef struct Dav1dLogger {
 } Dav1dLogger;
 
 typedef struct Dav1dSettings {
-    int n_frame_threads;
-    int n_tile_threads;
+    int n_threads;
+    int max_frame_delay; ///< internally clipped to $n_threads. Set to 1 for low-latency decoding.
     int apply_grain;
     int operating_point; ///< select an operating point for scalable AV1 bitstreams (0 - 31)
     int all_layers; ///< output all spatial layers of a scalable AV1 biststream
     unsigned frame_size_limit; ///< maximum frame size, in pixels (0 = unlimited)
     Dav1dPicAllocator allocator; ///< Picture allocator callback.
     Dav1dLogger logger; ///< Logger callback.
-    int n_postfilter_threads;
-    uint8_t reserved[28]; ///< reserved for future use
+    uint8_t reserved[32]; ///< reserved for future use
 } Dav1dSettings;
 
 /**

meson.build
@@ -30,7 +30,7 @@ project('dav1d', ['c'],
                       'b_ndebug=if-release'],
     meson_version: '>= 0.49.0')
 
-dav1d_soname_version       = '5.1.1'
+dav1d_soname_version       = '6.0.0'
 dav1d_api_version_array    = dav1d_soname_version.split('.')
 dav1d_api_version_major    = dav1d_api_version_array[0]
 dav1d_api_version_minor    = dav1d_api_version_array[1]

src/cdf.c
@@ -4096,16 +4096,15 @@ void dav1d_cdf_thread_copy(CdfContext *const dst, const CdfThreadContext *const 
 }
 
 int dav1d_cdf_thread_alloc(Dav1dContext *const c, CdfThreadContext *const cdf,
-                           struct thread_data *const t)
+                           const int have_frame_mt)
 {
     cdf->ref = dav1d_ref_create_using_pool(c->cdf_pool,
                                            sizeof(CdfContext) + sizeof(atomic_uint));
     if (!cdf->ref) return DAV1D_ERR(ENOMEM);
     cdf->data.cdf = cdf->ref->data;
-    if (t) {
+    if (have_frame_mt) {
         cdf->progress = (atomic_uint *) &cdf->data.cdf[1];
         atomic_init(cdf->progress, 0);
-        cdf->t = t;
     }
     return 0;
 }
@@ -4122,23 +4121,4 @@ void dav1d_cdf_thread_unref(CdfThreadContext *const cdf) {
     if (cdf->ref)
         dav1d_ref_dec(&cdf->ref);
     memset(cdf, 0, sizeof(*cdf));
-}
-
-void dav1d_cdf_thread_wait(CdfThreadContext *const cdf) {
-    if (!cdf->t) return;
-
-    if (atomic_load(cdf->progress)) return;
-    pthread_mutex_lock(&cdf->t->lock);
-    while (!atomic_load(cdf->progress))
-        pthread_cond_wait(&cdf->t->cond, &cdf->t->lock);
-    pthread_mutex_unlock(&cdf->t->lock);
-}
-
-void dav1d_cdf_thread_signal(CdfThreadContext *const cdf) {
-    if (!cdf->t) return;
-
-    pthread_mutex_lock(&cdf->t->lock);
-    atomic_store(cdf->progress, 1);
-    pthread_cond_broadcast(&cdf->t->cond);
-    pthread_mutex_unlock(&cdf->t->lock);
 }

src/cdf.h
@@ -135,23 +135,16 @@ typedef struct CdfThreadContext {
         CdfContext *cdf; // if ref != NULL
         unsigned qcat; // if ref == NULL, from static CDF tables
     } data;
-    struct thread_data *t;
     atomic_uint *progress;
 } CdfThreadContext;
 
 void dav1d_cdf_thread_init_static(CdfThreadContext *cdf, int qidx);
 int dav1d_cdf_thread_alloc(Dav1dContext *c, CdfThreadContext *cdf,
-                           struct thread_data *t);
+                           const int have_frame_mt);
 void dav1d_cdf_thread_copy(CdfContext *dst, const CdfThreadContext *src);
 void dav1d_cdf_thread_ref(CdfThreadContext *dst, CdfThreadContext *src);
 void dav1d_cdf_thread_unref(CdfThreadContext *cdf);
 void dav1d_cdf_thread_update(const Dav1dFrameHeader *hdr, CdfContext *dst,
                              const CdfContext *src);
-
-/*
- * These are binary signals (so a signal is either "done" or "not done").
- */
-void dav1d_cdf_thread_wait(CdfThreadContext *cdf);
-void dav1d_cdf_thread_signal(CdfThreadContext *cdf);
 
 #endif /* DAV1D_SRC_CDF_H */

commit cce2b0564d3b0aa1a5e4ec0c561b6a1b8497cebb

Fix frame context pthread_cond leak introduced by 753eef83

src/lib.c
@@ -594,6 +594,8 @@ static COLD void close_internal(Dav1dContext **const c_out, int flush) {
             freep(&f->frame_thread.tile_start_off);
             dav1d_freep_aligned(&f->frame_thread.pal);
             freep(&f->frame_thread.cbi);
+        }
+        if (c->n_tc > 1) {
             pthread_cond_destroy(&f->task_thread.cond);
         }
         freep(&f->frame_thread.frame_progress);