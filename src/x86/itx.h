1e602b8b3395d53d35bbd2eddd74fb1abd3c2f18
Matthias Dressel <code@deadcode.eu>
2022-04-25 18:54:38 +0200

    x86/itx: Add 32x16 12bpc AVX2 idtx
    inv_txfm_add_32x16_identity_identity_0_12bpc_c:      4138.7 ( 1.00x)
    inv_txfm_add_32x16_identity_identity_0_12bpc_avx2:     30.4 (136.26x)
    inv_txfm_add_32x16_identity_identity_1_12bpc_c:      4147.5 ( 1.00x)
    inv_txfm_add_32x16_identity_identity_1_12bpc_avx2:     30.7 (135.25x)
    inv_txfm_add_32x16_identity_identity_2_12bpc_c:      4138.2 ( 1.00x)
    inv_txfm_add_32x16_identity_identity_2_12bpc_avx2:     98.9 (41.84x)
    inv_txfm_add_32x16_identity_identity_3_12bpc_c:      4136.6 ( 1.00x)
    inv_txfm_add_32x16_identity_identity_3_12bpc_avx2:    167.7 (24.67x)
    inv_txfm_add_32x16_identity_identity_4_12bpc_c:      4156.3 ( 1.00x)
    inv_txfm_add_32x16_identity_identity_4_12bpc_avx2:    242.1 (17.17x)

--- src/x86/itx.h
+++ src/x86/itx.h
@@ -318,6 +318,7 @@ static ALWAYS_INLINE void itx_dsp_init_x86(Dav1dInvTxfmDSPContext *const c, cons
         assign_itx12_bpc_fn( , 16, 16, 12, avx2);
         assign_itx2_bpc_fn (R, 32,  8, 12, avx2);
         assign_itx_bpc_fn(R, 16, 32, identity_identity, IDTX, 12, avx2);
+        assign_itx_bpc_fn(R, 32, 16, identity_identity, IDTX, 12, avx2);
     }
 #endif
 
