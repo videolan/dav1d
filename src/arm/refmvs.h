b33d77f90347d66edd6823ef2dc52e7b3e05ef83
Martin Storsjö <martin@martin.st>
2023-06-29 00:10:32 +0300

    arm32: refmvs: Add NEON implementation of save_tmvs
    Relative speedup compared to C:
                 Cortex A7     A8     A9    A53    A72    A73
    save_tmvs_neon:   1.20   1.42   1.25   1.58   1.26   1.99

--- src/arm/refmvs.h
+++ src/arm/refmvs.h
@@ -36,8 +36,6 @@ static ALWAYS_INLINE void refmvs_dsp_init_arm(Dav1dRefmvsDSPContext *const c) {
 
     if (!(flags & DAV1D_ARM_CPU_FLAG_NEON)) return;
 
-#if ARCH_AARCH64
     c->save_tmvs = dav1d_save_tmvs_neon;
-#endif
     c->splat_mv = dav1d_splat_mv_neon;
 }
