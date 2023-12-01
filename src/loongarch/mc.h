d61886753328cb11227580ed411ac5d889486d63
yuanhecai <yuanhecai@loongson.cn>
2023-12-01 11:08:09 +0800

    loongarch: Improve the performance of avg functions
    Relative speedup over C code:
    
    avg_w4_8bpc_c:                           7.0 ( 1.00x)
    avg_w4_8bpc_lsx:                         0.8 ( 8.69x)
    avg_w4_8bpc_lasx:                        0.8 ( 8.94x)
    avg_w8_8bpc_c:                          20.4 ( 1.00x)
    avg_w8_8bpc_lsx:                         1.1 (18.25x)
    avg_w8_8bpc_lasx:                        0.9 (23.16x)
    avg_w16_8bpc_c:                         65.1 ( 1.00x)
    avg_w16_8bpc_lsx:                        2.5 (26.43x)
    avg_w16_8bpc_lasx:                       2.0 (32.05x)
    avg_w32_8bpc_c:                        255.1 ( 1.00x)
    avg_w32_8bpc_lsx:                        8.6 (29.74x)
    avg_w32_8bpc_lasx:                       6.0 (42.80x)
    avg_w64_8bpc_c:                        611.0 ( 1.00x)
    avg_w64_8bpc_lsx:                       21.0 (29.10x)
    avg_w64_8bpc_lasx:                      12.1 (50.36x)
    avg_w128_8bpc_c:                      1519.3 ( 1.00x)
    avg_w128_8bpc_lsx:                      88.7 (17.13x)
    avg_w128_8bpc_lasx:                     60.3 (25.20x)

--- src/loongarch/mc.h
+++ src/loongarch/mc.h
@@ -32,12 +32,14 @@
 #include "src/mc.h"
 #include "src/cpu.h"
 
+decl_avg_fn(BF(dav1d_avg, lsx));
 decl_w_avg_fn(BF(dav1d_w_avg, lsx));
 decl_mask_fn(BF(dav1d_mask, lsx));
 decl_warp8x8_fn(BF(dav1d_warp_affine_8x8, lsx));
 decl_warp8x8t_fn(BF(dav1d_warp_affine_8x8t, lsx));
 decl_w_mask_fn(BF(dav1d_w_mask_420, lsx));
 
+decl_avg_fn(BF(dav1d_avg, lasx));
 decl_w_avg_fn(BF(dav1d_w_avg, lasx));
 decl_mask_fn(BF(dav1d_mask, lasx));
 decl_warp8x8_fn(BF(dav1d_warp_affine_8x8, lasx));
@@ -50,6 +52,7 @@ static ALWAYS_INLINE void mc_dsp_init_loongarch(Dav1dMCDSPContext *const c) {
 
     if (!(flags & DAV1D_LOONGARCH_CPU_FLAG_LSX)) return;
 
+    c->avg = BF(dav1d_avg, lsx);
     c->w_avg = BF(dav1d_w_avg, lsx);
     c->mask = BF(dav1d_mask, lsx);
     c->warp8x8 = BF(dav1d_warp_affine_8x8, lsx);
@@ -58,6 +61,7 @@ static ALWAYS_INLINE void mc_dsp_init_loongarch(Dav1dMCDSPContext *const c) {
 
     if (!(flags & DAV1D_LOONGARCH_CPU_FLAG_LASX)) return;
 
+    c->avg = BF(dav1d_avg, lasx);
     c->w_avg = BF(dav1d_w_avg, lasx);
     c->mask = BF(dav1d_mask, lasx);
     c->warp8x8 = BF(dav1d_warp_affine_8x8, lasx);
