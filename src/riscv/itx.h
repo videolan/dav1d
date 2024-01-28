802d9257c5905b65f7ef0d68da751cbda8b934a7
Nathan E. Egge <unlord@xiph.org>
2024-01-28 18:46:50 -0500

    riscv64/itx: Add 4x4 8bpc dct_identity and identity_dct
    inv_txfm_add_4x4_dct_identity_0_8bpc_c:      619.6 ( 1.00x)
    inv_txfm_add_4x4_dct_identity_0_8bpc_rvv:     95.6 ( 6.48x)
    inv_txfm_add_4x4_dct_identity_1_8bpc_c:      620.0 ( 1.00x)
    inv_txfm_add_4x4_dct_identity_1_8bpc_rvv:     95.6 ( 6.49x)
    inv_txfm_add_4x4_identity_dct_0_8bpc_c:      573.9 ( 1.00x)
    inv_txfm_add_4x4_identity_dct_0_8bpc_rvv:     98.1 ( 5.85x)
    inv_txfm_add_4x4_identity_dct_1_8bpc_c:      573.1 ( 1.00x)
    inv_txfm_add_4x4_identity_dct_1_8bpc_rvv:     98.1 ( 5.84x)

--- src/riscv/itx.h
+++ src/riscv/itx.h
@@ -104,6 +104,8 @@ static ALWAYS_INLINE void itx_dsp_init_riscv(Dav1dInvTxfmDSPContext *const c, in
 
 #if BITDEPTH == 8
   assign_itx2_fn( ,  4,  4, rvv);
+  assign_itx_fn( , 4, 4, dct_identity,      H_DCT,     rvv);
+  assign_itx_fn( , 4, 4, identity_dct,      V_DCT,     rvv);
   assign_itx_fn( , 8, 8, identity_identity, IDTX,      rvv);
 #endif
 }
