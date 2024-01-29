5305eb3632e7985c55c3178d354d8d0e963c3637
Nathan E. Egge <unlord@xiph.org>
2024-01-28 19:17:00 -0500

    riscv64/itx: Add 4-point 8bpc RVV adst transform
    inv_txfm_add_4x4_adst_adst_0_8bpc_c:          674.2 ( 1.00x)
    inv_txfm_add_4x4_adst_adst_0_8bpc_rvv:         98.5 ( 6.84x)
    inv_txfm_add_4x4_adst_adst_1_8bpc_c:          674.1 ( 1.00x)
    inv_txfm_add_4x4_adst_adst_1_8bpc_rvv:         98.5 ( 6.84x)
    inv_txfm_add_4x4_adst_dct_0_8bpc_c:           650.3 ( 1.00x)
    inv_txfm_add_4x4_adst_dct_0_8bpc_rvv:          93.1 ( 6.99x)
    inv_txfm_add_4x4_adst_dct_1_8bpc_c:           650.6 ( 1.00x)
    inv_txfm_add_4x4_adst_dct_1_8bpc_rvv:          93.1 ( 6.99x)
    inv_txfm_add_4x4_adst_identity_0_8bpc_c:      635.6 ( 1.00x)
    inv_txfm_add_4x4_adst_identity_0_8bpc_rvv:     79.9 ( 7.95x)
    inv_txfm_add_4x4_adst_identity_1_8bpc_c:      635.4 ( 1.00x)
    inv_txfm_add_4x4_adst_identity_1_8bpc_rvv:     79.9 ( 7.95x)
    inv_txfm_add_4x4_dct_adst_0_8bpc_c:           649.7 ( 1.00x)
    inv_txfm_add_4x4_dct_adst_0_8bpc_rvv:          93.7 ( 6.93x)
    inv_txfm_add_4x4_dct_adst_1_8bpc_c:           649.6 ( 1.00x)
    inv_txfm_add_4x4_dct_adst_1_8bpc_rvv:          93.7 ( 6.93x)
    inv_txfm_add_4x4_identity_adst_0_8bpc_c:      594.1 ( 1.00x)
    inv_txfm_add_4x4_identity_adst_0_8bpc_rvv:     79.8 ( 7.45x)
    inv_txfm_add_4x4_identity_adst_1_8bpc_c:      592.6 ( 1.00x)
    inv_txfm_add_4x4_identity_adst_1_8bpc_rvv:     79.8 ( 7.43x)

--- src/riscv/itx.h
+++ src/riscv/itx.h
@@ -104,8 +104,13 @@ static ALWAYS_INLINE void itx_dsp_init_riscv(Dav1dInvTxfmDSPContext *const c, in
 
 #if BITDEPTH == 8
   assign_itx2_fn( ,  4,  4, rvv);
+  assign_itx_fn( , 4, 4, dct_adst,          ADST_DCT,  rvv);
   assign_itx_fn( , 4, 4, dct_identity,      H_DCT,     rvv);
+  assign_itx_fn( , 4, 4, adst_dct,          DCT_ADST,  rvv);
+  assign_itx_fn( , 4, 4, adst_adst,         ADST_ADST, rvv);
   assign_itx_fn( , 4, 4, identity_dct,      V_DCT,     rvv);
+  assign_itx_fn( , 4, 4, adst_identity,     H_ADST,    rvv);
+  assign_itx_fn( , 4, 4, identity_adst,     V_ADST,    rvv);
   assign_itx_fn( , 8, 8, identity_identity, IDTX,      rvv);
 #endif
 }
