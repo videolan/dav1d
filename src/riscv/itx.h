cc29b2314ce4e350d08fe5714f6bf88d80cfc00b
Nathan E. Egge <unlord@xiph.org>
2024-02-11 21:48:55 -0500

    riscv64/itx: Add 16x16 8bpc dct_identity and identity_dct
    inv_txfm_add_16x16_dct_identity_0_8bpc_c:    10593.3 ( 1.00x)
    inv_txfm_add_16x16_dct_identity_0_8bpc_rvv:   1163.3 ( 9.11x)
    inv_txfm_add_16x16_dct_identity_1_8bpc_c:    10584.9 ( 1.00x)
    inv_txfm_add_16x16_dct_identity_1_8bpc_rvv:   1163.3 ( 9.10x)
    inv_txfm_add_16x16_dct_identity_2_8bpc_c:    10590.3 ( 1.00x)
    inv_txfm_add_16x16_dct_identity_2_8bpc_rvv:   1163.6 ( 9.10x)
    inv_txfm_add_16x16_identity_dct_0_8bpc_c:     9945.9 ( 1.00x)
    inv_txfm_add_16x16_identity_dct_0_8bpc_rvv:   1150.2 ( 8.65x)
    inv_txfm_add_16x16_identity_dct_1_8bpc_c:     9937.0 ( 1.00x)
    inv_txfm_add_16x16_identity_dct_1_8bpc_rvv:   1150.3 ( 8.64x)
    inv_txfm_add_16x16_identity_dct_2_8bpc_c:     9934.6 ( 1.00x)
    inv_txfm_add_16x16_identity_dct_2_8bpc_rvv:   1150.4 ( 8.64x)

--- src/riscv/itx.h
+++ src/riscv/itx.h
@@ -107,5 +107,7 @@ static ALWAYS_INLINE void itx_dsp_init_riscv(Dav1dInvTxfmDSPContext *const c, in
   assign_itx16_fn( ,  4,  4, rvv);
   assign_itx16_fn( ,  8,  8, rvv);
   assign_itx2_fn( , 16, 16, rvv);
+  assign_itx_fn( , 16, 16, dct_identity,      H_DCT,     rvv);
+  assign_itx_fn( , 16, 16, identity_dct,      V_DCT,     rvv);
 #endif
 }
