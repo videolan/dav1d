02309b9f60198fb122409692d8909711da3bf16b
guxiwei <guxiwei-hf@loongson.cn>
2024-06-25 19:26:39 +0800

    msac: Add msac_decode_bool_equia_lsx and msac_decode_hi_tok_lsx
    The performance data is as follows:
    msac_decode_bool_equi_c:             0.4 ( 1.00x)
    msac_decode_bool_equi_lsx:           0.3 ( 1.07x)
    msac_decode_hi_tok_c:                1.8 ( 1.00x)
    msac_decode_hi_tok_lsx:              1.4 ( 1.27x)
    
    Change-Id: Ic2f2678cf699bb22c579424af71ae2603e228482

--- tests/checkasm/msac.c
+++ tests/checkasm/msac.c
@@ -280,6 +280,8 @@ void checkasm_check_msac(void) {
         c.decode_symbol_adapt16 = dav1d_msac_decode_symbol_adapt16_lsx;
         c.decode_bool_adapt     = dav1d_msac_decode_bool_adapt_lsx;
         c.decode_bool           = dav1d_msac_decode_bool_lsx;
+        c.decode_bool_equi      = dav1d_msac_decode_bool_equi_lsx;
+        c.decode_hi_tok         = dav1d_msac_decode_hi_tok_lsx;
     }
 #elif ARCH_X86 && HAVE_ASM
     if (dav1d_get_cpu_flags() & DAV1D_X86_CPU_FLAG_SSE2) {
