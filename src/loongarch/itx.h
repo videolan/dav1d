5de878a4e1741ff8ab1a0a8949f9d30518b3914d
yuanhecai <yuanhecai@loongson.cn>
2024-06-25 16:35:24 +0800

    loongarch: Minor improvement on identity4*, identity8* and dct32*
    1. remove the code about identity8 in the 4x8/8x8/8x16 series
    2. modify the code of the function dct_dct_8x32/32x32/64x64
    3. modify the code about identity4 in the 4x4/4x8/8x4 series
    
    After the modification, function performance has been improved by 20%
    
    Change-Id: I1bc2e0fb25e508faf9fc220333460a99be3f5e49

--- src/loongarch/itx.h
+++ src/loongarch/itx.h
@@ -277,7 +277,7 @@ static ALWAYS_INLINE void itx_dsp_init_loongarch(Dav1dInvTxfmDSPContext *const c
 
     c->itxfm_add[RTX_8X32][DCT_DCT] = dav1d_inv_txfm_add_dct_dct_8x32_8bpc_lsx;
 
-    //c->itxfm_add[RTX_16X32][DCT_DCT] = dav1d_inv_txfm_add_dct_dct_16x32_8bpc_lsx;
+    c->itxfm_add[RTX_16X32][DCT_DCT] = dav1d_inv_txfm_add_dct_dct_16x32_8bpc_lsx;
 
     c->itxfm_add[TX_32X32][DCT_DCT] = dav1d_inv_txfm_add_dct_dct_32x32_8bpc_lsx;
 
