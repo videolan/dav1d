2a9cbcc2f38a38e733d7834298326f0bbfa23608
pengxu <pengxu@loongson.cn>
2024-07-03 15:19:13 +0800

    Loongarch: Optimized ipred_dc,ipred_dc_128 8bpc,ipred_dc_left and ipred_dc_top functions by LSX
    intra_pred_dc_w4_8bpc_c:              2.1 ( 1.00x)
    intra_pred_dc_w4_8bpc_lsx:            1.3 ( 1.54x)
    intra_pred_dc_w8_8bpc_c:              3.6 ( 1.00x)
    intra_pred_dc_w8_8bpc_lsx:            3.7 ( 0.97x)
    intra_pred_dc_w16_8bpc_c:             6.9 ( 1.00x)
    intra_pred_dc_w16_8bpc_lsx:           7.8 ( 0.88x)
    intra_pred_dc_w32_8bpc_c:            14.1 ( 1.00x)
    intra_pred_dc_w32_8bpc_lsx:           7.1 ( 1.97x)
    intra_pred_dc_w64_8bpc_c:            25.3 ( 1.00x)
    intra_pred_dc_w64_8bpc_lsx:           7.4 ( 3.41x)
    intra_pred_dc_128_w4_8bpc_c:          0.6 ( 1.00x)
    intra_pred_dc_128_w4_8bpc_lsx:        0.8 ( 0.76x)
    intra_pred_dc_128_w8_8bpc_c:          1.4 ( 1.00x)
    intra_pred_dc_128_w8_8bpc_lsx:        3.2 ( 0.45x)
    intra_pred_dc_128_w16_8bpc_c:         3.4 ( 1.00x)
    intra_pred_dc_128_w16_8bpc_lsx:       7.3 ( 0.47x)
    intra_pred_dc_128_w32_8bpc_c:         8.8 ( 1.00x)
    intra_pred_dc_128_w32_8bpc_lsx:       6.4 ( 1.38x)
    intra_pred_dc_128_w64_8bpc_c:        17.0 ( 1.00x)
    intra_pred_dc_128_w64_8bpc_lsx:       6.2 ( 2.74x)
    intra_pred_dc_left_w4_8bpc_c:         1.1 ( 1.00x)
    intra_pred_dc_left_w4_8bpc_lsx:       1.1 ( 1.00x)
    intra_pred_dc_left_w8_8bpc_c:         2.1 ( 1.00x)
    intra_pred_dc_left_w8_8bpc_lsx:       3.4 ( 0.64x)
    intra_pred_dc_left_w16_8bpc_c:        4.6 ( 1.00x)
    intra_pred_dc_left_w16_8bpc_lsx:      7.5 ( 0.62x)
    intra_pred_dc_left_w32_8bpc_c:       10.3 ( 1.00x)
    intra_pred_dc_left_w32_8bpc_lsx:      7.8 ( 1.32x)
    intra_pred_dc_left_w64_8bpc_c:       18.7 ( 1.00x)
    intra_pred_dc_left_w64_8bpc_lsx:      6.6 ( 2.83x)
    intra_pred_dc_top_w4_8bpc_c:          0.9 ( 1.00x)
    intra_pred_dc_top_w4_8bpc_lsx:        0.8 ( 1.10x)
    intra_pred_dc_top_w8_8bpc_c:          1.9 ( 1.00x)
    intra_pred_dc_top_w8_8bpc_lsx:        2.8 ( 0.67x)
    intra_pred_dc_top_w16_8bpc_c:         4.2 ( 1.00x)
    intra_pred_dc_top_w16_8bpc_lsx:       5.5 ( 0.77x)
    intra_pred_dc_top_w32_8bpc_c:        10.4 ( 1.00x)
    intra_pred_dc_top_w32_8bpc_lsx:       6.7 ( 1.54x)
    intra_pred_dc_top_w64_8bpc_c:        19.9 ( 1.00x)
    intra_pred_dc_top_w64_8bpc_lsx:       6.9 ( 2.87x)
    
    Change-Id: Ib5349e2430302da0424a474ce0fedc457439c761

---
+++ src/loongarch/ipred.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2024, VideoLAN and dav1d authors
+ * Copyright © 2024, Loongson Technology Corporation Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef DAV1D_SRC_LOONGARCH_IPRED_H
+#define DAV1D_SRC_LOONGARCH_IPRED_H
+
+#include "config.h"
+#include "src/ipred.h"
+#include "src/cpu.h"
+#include "src/tables.h"
+
+#define MULTIPLIER_1x2 0x5556
+#define MULTIPLIER_1x4 0x3334
+#define BASE_SHIFT 16
+
+#define init_fn(type0, type1, name, suffix) \
+    c->type0[type1] = BF(dav1d_##name, suffix)
+
+#define init_angular_ipred_fn(type, name, suffix) \
+    init_fn(intra_pred, type, name, suffix)
+
+decl_angular_ipred_fn(BF(dav1d_ipred_dc, lsx));
+decl_angular_ipred_fn(BF(dav1d_ipred_dc_128, lsx));
+decl_angular_ipred_fn(BF(dav1d_ipred_dc_top, lsx));
+decl_angular_ipred_fn(BF(dav1d_ipred_dc_left, lsx));
+
+static ALWAYS_INLINE void intra_pred_dsp_init_loongarch(Dav1dIntraPredDSPContext *const c) {
+    const unsigned flags = dav1d_get_cpu_flags();
+
+    if (!(flags & DAV1D_LOONGARCH_CPU_FLAG_LSX)) return;
+
+#if BITDEPTH == 8
+    init_angular_ipred_fn(DC_PRED,       ipred_dc,       lsx);
+    init_angular_ipred_fn(DC_128_PRED,   ipred_dc_128,   lsx);
+    init_angular_ipred_fn(TOP_DC_PRED,   ipred_dc_top,   lsx);
+    init_angular_ipred_fn(LEFT_DC_PRED,  ipred_dc_left,  lsx);
+#endif
+}
+
+#endif /* DAV1D_SRC_LOONGARCH_IPRED_H */
