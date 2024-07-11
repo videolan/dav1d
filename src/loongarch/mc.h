b26f315d0055997942650cada6ca974321786551
jinbo <jinbo@loongson.cn>
2024-07-11 19:38:27 +0800

    loongarch: Add prep_8tap_8bpc_lsx
    mct_8tap_regular_w4_0_8bpc_c:                        3.7 ( 1.00x)
    mct_8tap_regular_w4_0_8bpc_lsx:                      0.9 ( 4.21x)
    mct_8tap_regular_w4_h_8bpc_c:                       15.7 ( 1.00x)
    mct_8tap_regular_w4_h_8bpc_lsx:                      1.7 ( 9.24x)
    mct_8tap_regular_w4_hv_8bpc_c:                      44.1 ( 1.00x)
    mct_8tap_regular_w4_hv_8bpc_lsx:                     6.3 ( 6.96x)
    mct_8tap_regular_w4_v_8bpc_c:                       19.8 ( 1.00x)
    mct_8tap_regular_w4_v_8bpc_lsx:                      2.4 ( 8.21x)
    mct_8tap_regular_w8_0_8bpc_c:                       10.5 ( 1.00x)
    mct_8tap_regular_w8_0_8bpc_lsx:                      1.3 ( 8.27x)
    mct_8tap_regular_w8_h_8bpc_c:                       47.2 ( 1.00x)
    mct_8tap_regular_w8_h_8bpc_lsx:                      6.2 ( 7.61x)
    mct_8tap_regular_w8_hv_8bpc_c:                     119.5 ( 1.00x)
    mct_8tap_regular_w8_hv_8bpc_lsx:                    18.9 ( 6.32x)
    mct_8tap_regular_w8_v_8bpc_c:                       60.5 ( 1.00x)
    mct_8tap_regular_w8_v_8bpc_lsx:                      5.4 (11.12x)
    mct_8tap_regular_w16_0_8bpc_c:                      28.8 ( 1.00x)
    mct_8tap_regular_w16_0_8bpc_lsx:                     2.8 (10.32x)
    mct_8tap_regular_w16_h_8bpc_c:                     151.9 ( 1.00x)
    mct_8tap_regular_w16_h_8bpc_lsx:                    19.8 ( 7.67x)
    mct_8tap_regular_w16_hv_8bpc_c:                    357.5 ( 1.00x)
    mct_8tap_regular_w16_hv_8bpc_lsx:                   57.6 ( 6.21x)
    mct_8tap_regular_w16_v_8bpc_c:                     195.6 ( 1.00x)
    mct_8tap_regular_w16_v_8bpc_lsx:                    16.9 (11.61x)
    mct_8tap_regular_w32_0_8bpc_c:                     104.6 ( 1.00x)
    mct_8tap_regular_w32_0_8bpc_lsx:                    11.6 ( 9.03x)
    mct_8tap_regular_w32_h_8bpc_c:                     596.3 ( 1.00x)
    mct_8tap_regular_w32_h_8bpc_lsx:                    77.8 ( 7.67x)
    mct_8tap_regular_w32_hv_8bpc_c:                   1329.0 ( 1.00x)
    mct_8tap_regular_w32_hv_8bpc_lsx:                  217.9 ( 6.10x)
    mct_8tap_regular_w32_v_8bpc_c:                     771.0 ( 1.00x)
    mct_8tap_regular_w32_v_8bpc_lsx:                    65.7 (11.73x)
    mct_8tap_regular_w64_0_8bpc_c:                     242.0 ( 1.00x)
    mct_8tap_regular_w64_0_8bpc_lsx:                    27.0 ( 8.95x)
    mct_8tap_regular_w64_h_8bpc_c:                    1455.9 ( 1.00x)
    mct_8tap_regular_w64_h_8bpc_lsx:                   186.9 ( 7.79x)
    mct_8tap_regular_w64_hv_8bpc_c:                   3221.7 ( 1.00x)
    mct_8tap_regular_w64_hv_8bpc_lsx:                  521.8 ( 6.17x)
    mct_8tap_regular_w64_v_8bpc_c:                    1836.1 ( 1.00x)
    mct_8tap_regular_w64_v_8bpc_lsx:                   158.2 (11.61x)
    mct_8tap_regular_w128_0_8bpc_c:                    629.0 ( 1.00x)
    mct_8tap_regular_w128_0_8bpc_lsx:                   66.3 ( 9.49x)
    mct_8tap_regular_w128_h_8bpc_c:                   3617.5 ( 1.00x)
    mct_8tap_regular_w128_h_8bpc_lsx:                  463.6 ( 7.80x)
    mct_8tap_regular_w128_hv_8bpc_c:                  7881.7 ( 1.00x)
    mct_8tap_regular_w128_hv_8bpc_lsx:                1290.3 ( 6.11x)
    mct_8tap_regular_w128_v_8bpc_c:                   4552.9 ( 1.00x)
    mct_8tap_regular_w128_v_8bpc_lsx:                  391.1 (11.64x)
    
    Change-Id: I8c6046e4bd6c1fb19d5712234abece0355fb77fa

--- src/loongarch/mc.h
+++ src/loongarch/mc.h
@@ -65,6 +65,16 @@ decl_warp8x8t_fn(BF(dav1d_warp_affine_8x8t, lasx));
 decl_w_mask_fn(BF(dav1d_w_mask_420, lasx));
 decl_blend_dir_fn(BF(dav1d_blend_h, lasx));
 
+decl_mct_fn(BF(dav1d_prep_8tap_regular,        lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_regular_smooth, lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_regular_sharp,  lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_smooth,         lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_smooth_regular, lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_smooth_sharp,   lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_sharp,          lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_sharp_regular,  lsx));
+decl_mct_fn(BF(dav1d_prep_8tap_sharp_smooth,   lsx));
+
 decl_mct_fn(BF(dav1d_prep_8tap_regular,        lasx));
 decl_mct_fn(BF(dav1d_prep_8tap_regular_smooth, lasx));
 decl_mct_fn(BF(dav1d_prep_8tap_regular_sharp,  lasx));
@@ -100,6 +110,16 @@ static ALWAYS_INLINE void mc_dsp_init_loongarch(Dav1dMCDSPContext *const c) {
     init_mc_fn(FILTER_2D_8TAP_SHARP_REGULAR,   8tap_sharp_regular,  lsx);
     init_mc_fn(FILTER_2D_8TAP_SHARP_SMOOTH,    8tap_sharp_smooth,   lsx);
     init_mc_fn(FILTER_2D_8TAP_SHARP,           8tap_sharp,          lsx);
+
+    init_mct_fn(FILTER_2D_8TAP_REGULAR,        8tap_regular,        lsx);
+    init_mct_fn(FILTER_2D_8TAP_REGULAR_SMOOTH, 8tap_regular_smooth, lsx);
+    init_mct_fn(FILTER_2D_8TAP_REGULAR_SHARP,  8tap_regular_sharp,  lsx);
+    init_mct_fn(FILTER_2D_8TAP_SMOOTH_REGULAR, 8tap_smooth_regular, lsx);
+    init_mct_fn(FILTER_2D_8TAP_SMOOTH,         8tap_smooth,         lsx);
+    init_mct_fn(FILTER_2D_8TAP_SMOOTH_SHARP,   8tap_smooth_sharp,   lsx);
+    init_mct_fn(FILTER_2D_8TAP_SHARP_REGULAR,  8tap_sharp_regular,  lsx);
+    init_mct_fn(FILTER_2D_8TAP_SHARP_SMOOTH,   8tap_sharp_smooth,   lsx);
+    init_mct_fn(FILTER_2D_8TAP_SHARP,          8tap_sharp,          lsx);
 
     if (!(flags & DAV1D_LOONGARCH_CPU_FLAG_LASX)) return;
 
