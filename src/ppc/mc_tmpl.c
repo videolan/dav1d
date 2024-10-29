08681fdf13e143d5b9e23aa5cc7caaee3aa5465a
Luca Barbato <lu_zero@gentoo.org>
2024-10-29 15:05:55 +0000

    ppc: blend pwr9 implementation
    blend_w4_8bpc_pwr9:      14.4 ( 1.90x)
    blend_w8_8bpc_pwr9:      19.9 ( 3.62x)
    blend_w16_8bpc_pwr9:     50.6 ( 5.17x)
    blend_w32_8bpc_pwr9:    125.8 ( 5.33x)

---
+++ src/ppc/mc_tmpl.c
@@ -0,0 +1,255 @@
+/*
+ * Copyright © 2024, VideoLAN and dav1d authors
+ * Copyright © 2024, Luca Barbato
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "src/ppc/mc.h"
+#include "src/tables.h"
+#include "src/ppc/dav1d_types.h"
+
+#if BITDEPTH == 8
+
+typedef void (*blend_line)(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride);
+
+#define BLEND_LINES(d0_u16, d1_u16, d2_u16, d3_u16, ab0, ab1, ab2, ab3, nm_m0, nm_m1, nm_m2, nm_m3) \
+{ \
+    u16x8 anm0 = vec_mule(ab0, nm_m0); \
+    u16x8 anm1 = vec_mule(ab1, nm_m1); \
+    u16x8 anm2 = vec_mule(ab2, nm_m2); \
+    u16x8 anm3 = vec_mule(ab3, nm_m3); \
+\
+    u16x8 bm0 = vec_mulo(ab0, nm_m0); \
+    u16x8 bm1 = vec_mulo(ab1, nm_m1); \
+    u16x8 bm2 = vec_mulo(ab2, nm_m2); \
+    u16x8 bm3 = vec_mulo(ab3, nm_m3); \
+\
+    d0_u16 = vec_add(anm0, bm0); \
+    d1_u16 = vec_add(anm1, bm1); \
+    d2_u16 = vec_add(anm2, bm2); \
+    d3_u16 = vec_add(anm3, bm3); \
+\
+    d0_u16 = vec_add(d0_u16, vec_splats((uint16_t)32)); \
+    d1_u16 = vec_add(d1_u16, vec_splats((uint16_t)32)); \
+    d2_u16 = vec_add(d2_u16, vec_splats((uint16_t)32)); \
+    d3_u16 = vec_add(d3_u16, vec_splats((uint16_t)32)); \
+\
+    d0_u16 = vec_sr(d0_u16, vec_splat_u16(6)); \
+    d1_u16 = vec_sr(d1_u16, vec_splat_u16(6)); \
+    d2_u16 = vec_sr(d2_u16, vec_splat_u16(6)); \
+    d3_u16 = vec_sr(d3_u16, vec_splat_u16(6)); \
+}
+
+static void blend4(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride)
+{
+    u8x16 v64u8 = vec_splats((uint8_t)64);
+    u8x16 a0 = vec_xl(0, dst);
+    u8x16 a1 = vec_xl(0, dst + stride);
+    u8x16 a2 = vec_xl(0, dst + 2 * stride);
+    u8x16 a3 = vec_xl(0, dst + 3 * stride);
+    u8x16 m0 = vec_xl(0, mask);
+    u8x16 m1 = vec_xl(0, mask + 4);
+    u8x16 m2 = vec_xl(0, mask + 2 * 4);
+    u8x16 m3 = vec_xl(0, mask + 3 * 4);
+    u8x16 b0 = vec_xl(0, tmp);
+    u8x16 b1 = vec_xl(0, tmp + 4);
+    u8x16 b2 = vec_xl(0, tmp + 2 * 4);
+    u8x16 b3 = vec_xl(0, tmp + 3 * 4);
+
+    u8x16 nm0 = vec_sub(v64u8, m0);
+    u8x16 nm1 = vec_sub(v64u8, m1);
+    u8x16 nm2 = vec_sub(v64u8, m2);
+    u8x16 nm3 = vec_sub(v64u8, m3);
+
+    u8x16 ab0 = vec_mergeh(a0, b0); // a even, b odd
+    u8x16 ab1 = vec_mergeh(a1, b1); // a even, b odd
+    u8x16 ab2 = vec_mergeh(a2, b2); // a even, b odd
+    u8x16 ab3 = vec_mergeh(a3, b3); // a even, b odd
+    u8x16 nm_m0 = vec_mergeh(nm0, m0);
+    u8x16 nm_m1 = vec_mergeh(nm1, m1);
+    u8x16 nm_m2 = vec_mergeh(nm2, m2);
+    u8x16 nm_m3 = vec_mergeh(nm3, m3);
+
+    u16x8 d0_u16, d1_u16, d2_u16, d3_u16;
+
+    BLEND_LINES(d0_u16, d1_u16, d2_u16, d3_u16, ab0, ab1, ab2, ab3, nm_m0, nm_m1, nm_m2, nm_m3);
+
+    u8x16 d0 = (u8x16)vec_pack(d0_u16, d0_u16);
+    u8x16 d1 = (u8x16)vec_pack(d1_u16, d1_u16);
+    u8x16 d2 = (u8x16)vec_pack(d2_u16, d2_u16);
+    u8x16 d3 = (u8x16)vec_pack(d3_u16, d3_u16);
+
+    vec_xst_len(d0, dst, 4);
+    vec_xst_len(d1, dst + stride, 4);
+    vec_xst_len(d2, dst + 2 * stride, 4);
+    vec_xst_len(d3, dst + 3 * stride, 4);
+}
+
+static void blend8(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride)
+{
+    u8x16 v64u8 = vec_splats((uint8_t)64);
+    u8x16 a0 = vec_xl(0, dst);
+    u8x16 a1 = vec_xl(0, dst + stride);
+    u8x16 a2 = vec_xl(0, dst + 2 * stride);
+    u8x16 a3 = vec_xl(0, dst + 3 * stride);
+    u8x16 m0 = vec_xl(0, mask);
+    u8x16 m1 = vec_xl(0, mask + 8);
+    u8x16 m2 = vec_xl(0, mask + 2 * 8);
+    u8x16 m3 = vec_xl(0, mask + 3 * 8);
+    u8x16 b0 = vec_xl(0, tmp);
+    u8x16 b1 = vec_xl(0, tmp + 8);
+    u8x16 b2 = vec_xl(0, tmp + 2 * 8);
+    u8x16 b3 = vec_xl(0, tmp + 3 * 8);
+
+    u8x16 nm0 = vec_sub(v64u8, m0);
+    u8x16 nm1 = vec_sub(v64u8, m1);
+    u8x16 nm2 = vec_sub(v64u8, m2);
+    u8x16 nm3 = vec_sub(v64u8, m3);
+
+    u8x16 ab0 = vec_mergeh(a0, b0); // a even, b odd
+    u8x16 ab1 = vec_mergeh(a1, b1); // a even, b odd
+    u8x16 ab2 = vec_mergeh(a2, b2); // a even, b odd
+    u8x16 ab3 = vec_mergeh(a3, b3); // a even, b odd
+    u8x16 nm_m0 = vec_mergeh(nm0, m0);
+    u8x16 nm_m1 = vec_mergeh(nm1, m1);
+    u8x16 nm_m2 = vec_mergeh(nm2, m2);
+    u8x16 nm_m3 = vec_mergeh(nm3, m3);
+
+    u16x8 d0_u16, d1_u16, d2_u16, d3_u16;
+
+    BLEND_LINES(d0_u16, d1_u16, d2_u16, d3_u16, ab0, ab1, ab2, ab3, nm_m0, nm_m1, nm_m2, nm_m3);
+
+    u8x16 d0 = (u8x16)vec_pack(d0_u16, d0_u16);
+    u8x16 d1 = (u8x16)vec_pack(d1_u16, d1_u16);
+    u8x16 d2 = (u8x16)vec_pack(d2_u16, d2_u16);
+    u8x16 d3 = (u8x16)vec_pack(d3_u16, d3_u16);
+
+    vec_xst_len(d0, dst, 8);
+    vec_xst_len(d1, dst + stride, 8);
+    vec_xst_len(d2, dst + 2 * stride, 8);
+    vec_xst_len(d3, dst + 3 * stride, 8);
+}
+
+static inline void blend16_lines(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride, int mstride)
+{
+    u8x16 v64u8 = vec_splats((uint8_t)64);
+    u8x16 a0 = vec_xl(0, dst);
+    u8x16 a1 = vec_xl(0, dst + stride);
+    u8x16 a2 = vec_xl(0, dst + 2 * stride);
+    u8x16 a3 = vec_xl(0, dst + 3 * stride);
+    u8x16 m0 = vec_xl(0, mask);
+    u8x16 m1 = vec_xl(0, mask + mstride);
+    u8x16 m2 = vec_xl(0, mask + 2 * mstride);
+    u8x16 m3 = vec_xl(0, mask + 3 * mstride);
+    u8x16 b0 = vec_xl(0, tmp);
+    u8x16 b1 = vec_xl(0, tmp + mstride);
+    u8x16 b2 = vec_xl(0, tmp + 2 * mstride);
+    u8x16 b3 = vec_xl(0, tmp + 3 * mstride);
+
+    u8x16 nm0 = vec_sub(v64u8, m0);
+    u8x16 nm1 = vec_sub(v64u8, m1);
+    u8x16 nm2 = vec_sub(v64u8, m2);
+    u8x16 nm3 = vec_sub(v64u8, m3);
+
+    u8x16 ab0 = vec_mergeh(a0, b0);
+    u8x16 ab1 = vec_mergeh(a1, b1);
+    u8x16 ab2 = vec_mergeh(a2, b2);
+    u8x16 ab3 = vec_mergeh(a3, b3);
+
+    u8x16 nm_m0 = vec_mergeh(nm0, m0);
+    u8x16 nm_m1 = vec_mergeh(nm1, m1);
+    u8x16 nm_m2 = vec_mergeh(nm2, m2);
+    u8x16 nm_m3 = vec_mergeh(nm3, m3);
+
+    u16x8 d0h_u16, d1h_u16, d2h_u16, d3h_u16;
+    u16x8 d0l_u16, d1l_u16, d2l_u16, d3l_u16;
+
+    BLEND_LINES(d0h_u16, d1h_u16, d2h_u16, d3h_u16, ab0, ab1, ab2, ab3, nm_m0, nm_m1, nm_m2, nm_m3)
+
+    ab0 = vec_mergel(a0, b0);
+    ab1 = vec_mergel(a1, b1);
+    ab2 = vec_mergel(a2, b2);
+    ab3 = vec_mergel(a3, b3);
+
+    nm_m0 = vec_mergel(nm0, m0);
+    nm_m1 = vec_mergel(nm1, m1);
+    nm_m2 = vec_mergel(nm2, m2);
+    nm_m3 = vec_mergel(nm3, m3);
+
+    BLEND_LINES(d0l_u16, d1l_u16, d2l_u16, d3l_u16, ab0, ab1, ab2, ab3, nm_m0, nm_m1, nm_m2, nm_m3)
+
+    u8x16 d0 = (u8x16)vec_pack(d0h_u16, d0l_u16);
+    u8x16 d1 = (u8x16)vec_pack(d1h_u16, d1l_u16);
+    u8x16 d2 = (u8x16)vec_pack(d2h_u16, d2l_u16);
+    u8x16 d3 = (u8x16)vec_pack(d3h_u16, d3l_u16);
+
+    vec_xst(d0, 0,dst);
+    vec_xst(d1, 0,dst + stride);
+    vec_xst(d2, 0,dst + 2 * stride);
+    vec_xst(d3, 0,dst + 3 * stride);
+}
+
+static void blend16(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride)
+{
+    blend16_lines(dst, tmp, mask, stride, 16);
+}
+
+static void blend32(uint8_t *dst, const uint8_t *tmp, const uint8_t *mask, int stride)
+{
+    for (int i = 0; i < 2; i++, dst += 16, tmp += 16, mask += 16) {
+        blend16_lines(dst, tmp, mask, stride, 32);
+    }
+}
+
+void dav1d_blend_8bpc_pwr9(pixel *dst, const ptrdiff_t dst_stride, const pixel *tmp,
+                           const int w, int h, const uint8_t *mask)
+{
+    blend_line blend;
+
+    switch(w) {
+        case 4:
+            blend = blend4;
+            break;
+        case 8:
+            blend = blend8;
+            break;
+        case 16:
+            blend = blend16;
+            break;
+        case 32:
+            blend = blend32;
+            break;
+        default:
+            assert(0);
+    }
+
+    for (int y = 0; y < h; y+=4) {
+        blend(dst, tmp, mask, PXSTRIDE(dst_stride));
+        dst += 4 * PXSTRIDE(dst_stride);
+        tmp += 4 * w;
+        mask += 4 * w;
+    }
+}
+
+#endif // BITDEPTH
